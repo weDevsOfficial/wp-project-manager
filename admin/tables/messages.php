df93c0ff6f07bb1b3378d83aacfaf2c1e0d68e9f
Tareq Hasan <tareq1988@gmail.com>
2012-09-05 09:20:49 +0600

    messages post table updated by extending wp_posts_list_table

--- admin/tables/messages.php
+++ admin/tables/messages.php
@@ -1,168 +1,52 @@
 <?php
 
+if ( !class_exists( 'WP_Posts_List_Table' ) ) {
+    require_once ABSPATH . '/wp-admin/includes/class-wp-posts-list-table.php';
+}
+
 /**
- * Project Listing table
+ * Message Listing table
  *
  * @package Client Project Manager
  */
-class CPM_Message_List_Table extends WP_List_Table {
+class CPM_Message_List_Table extends WP_Posts_List_Table {
 
-    private $_single_project;
+    var $is_trash = false;
 
-    function __construct( $project_id = 0 ) {
-        parent::__construct( array(
-            'singular' => 'cpm_project_list',
-            'plural' => 'cpm_project_lists',
-            'ajax' => false
-        ) );
-
-        $this->_single_project = $project_id;
-
-        $this->_column_headers = array($this->get_columns(), array(), $this->get_sortable_columns());
+    function __construct() {
+        parent::__construct();
     }
 
-    /**
-     * Define the columns that are going to be used in the table
-     *
-     * @return array $columns, the array of columns to use with the table
-     */
-    function get_columns() {
-        $columns = array(
-            'cb' => '<input type="checkbox" />',
-            'title' => __( 'Title' )
-        );
-
-        if ( !$this->_single_project ) {
-            $columns['project'] = __( 'Project' );
-        }
-
-        $columns['reply_count'] = __( 'Reply Count' );
-        $columns['privacy'] = __( 'Visibility' );
-        $columns['created'] = __( 'Created' );
-        $columns['actions'] = __( 'Actions' );
-
-        return $columns;
-    }
-
-    /**
-     * Decide which columns to activate the sorting functionality on
-     *
-     * @return array $sortable, the array of columns that can be sorted by the user
-     */
-    function get_sortable_columns() {
-        $columns = array(
-            'title' => array('title', false),
-            'privacy' => array('privacy', false),
-            'created' => array('created', false)
-        );
-
-        if ( !$this->_single_project ) {
-            $columns['project'] = array('project_id', false);
-        }
-
-        return $columns;
-    }
-
-    function column_default( $item, $column_name ) {
-        switch ($column_name) {
-            case 'title':
-                $detail_link = admin_url( 'admin.php?page=cpm_messages&action=single&mid=' . $item->id );
-                echo '<strong><a class="row-title" href="' . $detail_link . '" title="Edit">' . stripslashes( $item->title ) . '</a></strong>';
-                break;
-
-            case 'client':
-                $user = get_user_by( 'id', $item->client );
-                $link = '#';
-                echo '<a href="' . $link . '" title="Edit">' . $user->display_name . '</a>';
-                break;
-
-            case 'project':
-                $link = admin_url( 'admin.php?page=cpm_projects&action=details&pid=' . $item->project_id );
-                echo '<a href="' . $link . '" title="Edit">' . $item->name . '</a>';
-                break;
-
-            case 'reply_count':
-                echo $item->reply_count;
-                break;
-
-            case 'created':
-                $date = mysql2date( get_option( 'date_format' ), $item->created );
-                $abbr = date_i18n( 'Y/m/d g:i:s A', strtotime( $item->created ) );
-                printf( '<abbr title="%s">%s</abbr>', $abbr, $date );
-                break;
-
-            case 'privacy':
-                echo ( $item->privacy == 0 ) ? __( 'Public', 'cpm' ) : __( 'Private', 'cpm' );
-                break;
-
-            case 'actions':
-                $edit_link = '#';
-                $del_link = '#';
-                printf( '<a class="cpm-get-message" data-id="%1$d" href="%2$s">' . __( 'Edit', 'cpm' ) . '</a> | <a class="cpm-del-message" data-id="%1$d" href="%3$s">' . __( 'Delete', 'cpm' ) . '</a>', $item->id, $edit_link, $del_link );
-                break;
-        }
-    }
-
-    function column_cb( $item ) {
-        return sprintf( '<input type="checkbox" name="%1$s[]" value="%2$s" />', $this->_args['singular'], $item->id );
-    }
-
-    /**
-     * Prepare the table with different parameters, pagination, columns and table elements
-     */
     function prepare_items() {
-        global $wpdb, $_wp_column_headers;
-        $screen = get_current_screen();
+        global $post_type_object, $avail_post_stati, $wp_query, $per_page, $mode;
 
-        /* -- Preparing your query -- */
-        $query = "SELECT m.*, p.name FROM " . CPM_MESSAGE_TABLE . " m
-                INNER JOIN " . CPM_PROJECT_TABLE . " p ON p.id = m.project_id";
+        $args = array(
+            'post_type' => 'message'
+        );
+        $avail_post_stati = wp_edit_posts_query( $args );
 
-        if ( $this->_single_project ) {
-            $query .= ' WHERE m.project_id = ' . $this->_single_project;
-        }
+        $this->hierarchical_display = ( $post_type_object->hierarchical && 'menu_order title' == $wp_query->query['orderby'] );
 
-        /* -- Ordering parameters -- */
-        //Parameters that are going to be used to order the result
-        $orderby = !empty( $_GET["orderby"] ) ? mysql_real_escape_string( $_GET["orderby"] ) : 'ASC';
-        $order = !empty( $_GET["order"] ) ? mysql_real_escape_string( $_GET["order"] ) : '';
-        if ( !empty( $orderby ) & !empty( $order ) ) {
-            $query.=' ORDER BY ' . $orderby . ' ' . $order;
-        }
+        $total_items = $this->hierarchical_display ? $wp_query->post_count : $wp_query->found_posts;
 
-        /* -- Pagination parameters -- */
-        //Number of elements in your table?
-        $totalitems = $wpdb->query( $query ); //return the total number of affected rows
-        //How many to display per page?
-        $perpage = 15;
-        //Which page is this?
-        $paged = !empty( $_GET["paged"] ) ? mysql_real_escape_string( $_GET["paged"] ) : '';
-        //Page Number
-        if ( empty( $paged ) || !is_numeric( $paged ) || $paged <= 0 ) {
-            $paged = 1;
-        }
-        //How many pages do we have in total?
-        $totalpages = ceil( $totalitems / $perpage );
-        //adjust the query to take pagination into account
-        if ( !empty( $paged ) && !empty( $perpage ) ) {
-            $offset = ($paged - 1) * $perpage;
-            $query.=' LIMIT ' . (int) $offset . ',' . (int) $perpage;
-        }
+        $post_type = $post_type_object->name;
+        $per_page = $this->get_items_per_page( 'edit_' . $post_type . '_per_page' );
+        $per_page = apply_filters( 'edit_posts_per_page', $per_page, $post_type );
 
-        /* -- Register the pagination -- */
+        if ( $this->hierarchical_display )
+            $total_pages = ceil( $total_items / $per_page );
+        else
+            $total_pages = $wp_query->max_num_pages;
+
+        $mode = empty( $_REQUEST['mode'] ) ? 'list' : $_REQUEST['mode'];
+
+        $this->is_trash = isset( $_REQUEST['post_status'] ) && $_REQUEST['post_status'] == 'trash';
+
         $this->set_pagination_args( array(
-            "total_items" => $totalitems,
-            "total_pages" => $totalpages,
-            "per_page" => $perpage,
+            'total_items' => $total_items,
+            'total_pages' => $total_pages,
+            'per_page' => $per_page
         ) );
-        //The pagination links are automatically built according to those parameters
-
-        /* -- Register the Columns -- */
-        $columns = $this->get_columns();
-        $_wp_column_headers[$screen->id] = $columns;
-
-        /* -- Fetch the items -- */
-        $this->items = $wpdb->get_results( $query );
     }
 
 }
