d279cdcccb9dcc684a944b43a032032334a9e0c7
Tareq Hasan <tareq1988@gmail.com>
2012-11-11 08:03:27 +0600

    task and invoice codes removed

--- js/admin.js
+++ js/admin.js
@@ -1,17 +1,10 @@
 ;(function ($) {
 
-    window.weDevs_CPM = {
+    var weDevs_CPM = {
         init: function () {
 
             //initialize uploader
             this.Uploader.init();
-
-            $('.cpm-form-tasks').on('click', '.cpm-add-task-item', this.Task.fieldAdd);
-            $('.cpm-form-tasks').on('click', '.cpm-remove-task-item', this.Task.fieldRemove);
-            $('.cpm-task').on('click', '.cpm-mark-task-complete', this.Task.markComplete);
-            $('.cpm-task').on('click', '.cpm-mark-task-open', this.Task.markOpen);
-            $('.cpm-task').on('click', '.cpm-mark-task-delete', this.Task.markDelete);
-            $('.cpm-task-list').on('click', '.cpm-hide-tasks', this.Task.toggleTasks);
 
             $('.cpm-links').on('click', '.cpm-milestone-delete', this.Milestone.remove);
             $('.cpm-links').on('click', '.cpm-milestone-complete', this.Milestone.markComplete);
@@ -43,63 +36,6 @@
 
             $('#cpm-upload-container').on('click', '.cpm-delete-file', weDevs_CPM.Uploader.deleteFile);
             $('.cpm-comment-wrap').on('click', '.cpm-delete-file', weDevs_CPM.Uploader.deleteFile);
-
-            // add/update new invoice
-            $('.cpm-new-invoice, .cpm-update-invoice').validate({
-                submitHandler: function(form) {
-                    weDevs_CPM.Invoice.submit.call(form);
-                }
-            });
-        },
-        Task: {
-            fieldAdd: function () {
-                var row = $('#cpm-form-tasks-new').html();
-
-                $(row).insertAfter($(this).parent().parent());
-                $(".datepicker").datepicker();
-
-                return false;
-            },
-            fieldRemove: function () {
-                $(this).parent().parent().remove();
-
-                return false;
-            },
-            markComplete: function (e) {
-                e.preventDefault();
-
-                weDevs_CPM.Task.ajaxRequest.call(this, 'cpm_task_complete');
-            },
-            markOpen: function (e) {
-                e.preventDefault();
-
-                weDevs_CPM.Task.ajaxRequest.call(this, 'cpm_task_open');
-            },
-            markDelete: function (e) {
-                e.preventDefault();
-
-                if (confirm('Are you sure?')) {
-                    weDevs_CPM.Task.ajaxRequest.call(this, 'cpm_task_delete');
-                }
-            },
-            ajaxRequest: function (action) {
-                var that = $(this),
-                data = {
-                    task_id: that.data('id'),
-                    action: action,
-                    '_wpnonce': CPM_Vars.nonce
-                };
-
-                that.addClass('cpm-loading');
-                $.post(ajaxurl, data, function (response) {
-                    location.reload();
-                });
-            },
-            toggleTasks: function (e) {
-                e.preventDefault();
-
-                $(this).parent().next('.cpm-tasks').slideToggle();
-            }
         },
         Milestone: {
             remove: function (e) {
@@ -166,12 +102,12 @@
                 e.preventDefault();
 
                 var that = $(this),
-                    parent = that.closest('.cpm-comment'),
-                    data = {
-                        id: that.data('id'),
-                        action: 'cpm_get_comment',
-                        '_wpnonce': CPM_Vars.nonce
-                    };
+                parent = that.closest('.cpm-comment'),
+                data = {
+                    id: that.data('id'),
+                    action: 'cpm_get_comment',
+                    '_wpnonce': CPM_Vars.nonce
+                };
 
                 //console.log(data);
                 $.post(CPM_Vars.ajaxurl, data, function(response) {
@@ -254,7 +190,6 @@
         weDevs_CPM.init();
 
         $('#project_manager, #project_coworker, #project_client').chosen();
-        $("#project_started, #project_ends").datepicker();
         $(".datepicker").datepicker();
 
         $("#project-status-slider").slider({
