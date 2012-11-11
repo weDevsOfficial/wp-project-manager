89a985e6cd0f0087966ecb99c8ad437c88d5a3cf
Tareq Hasan <tareq1988@gmail.com>
2012-11-12 04:41:34 +0600

    show comment and attachment function moved to here. other function buffering fix

--- includes/html.php
+++ includes/html.php
@@ -9,9 +9,11 @@
  * @return string
  */
 function cpm_task_html( $task, $project_id, $list_id, $single = false ) {
+    $wrap_class = ( $task->completed == '1' ) ? 'cpm-task-complete' : 'cpm-task-uncomplete';
+
     ob_start();
     ?>
-    <div class="cpm-todo-wrap">
+    <div class="cpm-todo-wrap <?php echo $wrap_class; ?>">
         <span class="cpm-todo-action">
             <a href="#" data-task_id="<?php echo $task->ID; ?>" data-confirm="<?php esc_attr_e( 'Are you sure to delete this to-do?', 'cpm' ); ?>" class="cpm-todo-delete">
                 <?php _e( 'Delete', 'cpm' ); ?>
@@ -223,7 +225,7 @@ function cpm_task_list_html( $list, $project_id ) {
             <h3><a href="<?php echo cpm_url_single_tasklist( $project_id, $list->ID ); ?>"><?php echo get_the_title( $list->ID ); ?></a></h3>
 
             <div class="cpm-entry-detail">
-                <?php echo cpm_print_content( $list->post_content ); ?>
+                <?php echo cpm_get_content( $list->post_content ); ?>
             </div>
         </header>
         <div class="cpm-list-edit-form">
@@ -281,22 +283,34 @@ function cpm_task_list_html( $list, $project_id ) {
  * @param string $type MESSAGE, TASK, TASK_LIST
  * @param type $privacy
  */
-function cpm_comment_form( $project_id, $object_id, $privacy = true ) {
+function cpm_comment_form( $project_id, $object_id = 0, $comment = null ) {
+    $action = 'cpm_comment_new';
+    $text = '';
+    $submit_button = __( 'Add this comment', 'cpm' );
+
+    if ( $comment ) {
+        $action = 'cpm_comment_update';
+        $text = $comment->comment_content;
+        $submit_button = __( 'Update comment', 'cpm' );
+    }
+
+    ob_start();
     ?>
     <div class="cpm-comment-form-wrap">
-        
-        <div class="cpm-avatar"><?php echo cpm_url_user( get_current_user_id(), true ); ?></div>
+
+        <?php if( !$comment ) { ?>
+            <div class="cpm-avatar"><?php echo cpm_url_user( get_current_user_id(), true ); ?></div>
+        <?php } ?>
 
         <form class="cpm-comment-form">
 
             <?php wp_nonce_field( 'cpm_new_message' ); ?>
 
             <div class="item message">
-                <textarea name="cpm_message" class="required" cols="55" rows="8" placeholder="<?php esc_attr_e( 'Add a comment...', 'cpm' ); ?>"></textarea>
+                <textarea name="cpm_message" class="required" cols="55" rows="8" placeholder="<?php esc_attr_e( 'Add a comment...', 'cpm' ); ?>"><?php echo esc_textarea( $text ); ?></textarea>
             </div>
 
             <div class="cpm-attachment-area">
-                <!-- <legend>Attach Files</legend> -->
                 <?php cpm_upload_field(); ?>
             </div>
 
@@ -306,12 +320,74 @@ function cpm_comment_form( $project_id, $object_id, $privacy = true ) {
             </fieldset>
 
             <div class="submit">
-                <input type="hidden" name="action" value="cpm_new_comment" />
-                <input type="hidden" name="parent_id" value="<?php echo $object_id; ?>" />
-                <input type="hidden" name="project_id" value="<?php echo $project_id; ?>" />
-                <input type="submit" class="button-primary" name="cpm_new_comment" value="<?php esc_attr_e( 'Add this comment', 'cpm' ); ?>" id="" />
+                <input type="submit" class="button-primary" name="cpm_new_comment" value="<?php echo esc_attr( $submit_button ); ?>" id="" />
+
+                <?php if( $comment ) { ?>
+                    <input type="hidden" name="comment_id" value="<?php echo $comment->comment_ID; ?>" />
+                    <a href="#" class="cpm-comment-edit-cancel button"><?php _e( 'Cancel', 'wedevs' ); ?></a>
+                <?php } else { ?>
+                    <input type="hidden" name="parent_id" value="<?php echo $object_id; ?>" />
+                <?php } ?>
+
+                <input type="hidden" name="action" value="<?php echo $action; ?>" />
             </div>
         </form>
     </div>
     <?php
+
+    return ob_get_clean();
+}
+
+function cpm_show_comment( $comment, $project_id, $class = '' ) {
+
+    $class = empty( $class ) ? '' : ' ' . $class;
+
+    ob_start();
+    ?>
+    <li class="cpm-comment<?php echo $class; ?>" id="cpm-comment-<?php echo $comment->comment_ID; ?>">
+        <div class="cpm-avatar"><?php echo cpm_url_user( $comment->user_id, true ); ?></div>
+        <div class="cpm-comment-container">
+            <div class="cpm-comment-meta">
+                <span class="cpm-author"><?php echo cpm_url_user( $comment->user_id ); ?></span>
+                <span class="cpm-separator">|</span>
+                <span class="cpm-date"><?php echo cpm_get_date( $comment->comment_date, true ); ?></span>
+
+                <?php if( $comment->user_id == get_current_user_id() ) { ?>
+                    <span class="cpm-separator">|</span>
+                    <span class="cpm-edit-link"><a href="#" class="cpm-edit-comment-link" data-comment_id="<?php echo $comment->comment_ID; ?>"
+                        data-project_id="<?php echo $project_id; ?>" data-object_id="<?php echo $comment->comment_post_ID; ?>"><?php _e( 'Edit', 'cpm' ); ?></a></span>
+                    <span class="cpm-separator">|</span>
+                    <span class="cpm-delete-link"><a href="#" class="cpm-delete-comment-link" data-id="<?php echo $comment->comment_ID; ?>"><?php _e( 'Delete', 'cpm' ); ?></a></span>
+                <?php } ?>
+            </div>
+            <div class="cpm-comment-content">
+                <?php echo comment_text( $comment->comment_ID ); ?>
+
+                <?php echo cpm_show_attachments( $comment ); ?>
+            </div>
+
+            <div class="cpm-comment-edit-form"></div>
+        </div>
+    </li>
+    <?php
+
+    return ob_get_clean();
+}
+
+function cpm_show_attachments( $object ) {
+    ob_start();
+
+    if ( $object->files ) {
+        ?>
+        <ul class="cpm-attachments">
+            <?php
+            foreach ($object->files as $file) {
+                printf( '<li><a href="%s" target="_blank"><img src="%s" /></a></li>', $file['url'], $file['thumb'] );
+            }
+            ?>
+        </ul>
+        <?php
+    }
+
+    return ob_get_clean();
 }
