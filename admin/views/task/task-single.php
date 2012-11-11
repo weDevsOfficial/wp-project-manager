fef3eb0a51ceef4a5197f291a55ad92fb00610ca
Tareq Hasan <tareq1988@gmail.com>
2012-11-12 04:39:31 +0600

    even/odd class added on task single comment

--- admin/views/task/task-single.php
+++ admin/views/task/task-single.php
@@ -13,15 +13,20 @@ $task = $task_obj->get_task( $task_id );
     <?php echo cpm_task_html( $task, $project_id, $list->ID, true ); ?>
 </div>
 
-<h3>Comments:</h3>
+<h3 class="cpm-comment-title"><?php _e( 'Discuss this to-do', 'cpm' ); ?></h3>
 <ul class="cpm-comment-wrap">
     <?php
     $comments = $task_obj->get_task_comments( $task_id );
     if ( $comments ) {
+
+        $count = 0;
         foreach ($comments as $comment) {
-            cpm_show_comment( $comment );
+            $class = ( $count % 2 == 0 ) ? 'even' : 'odd';
+            echo cpm_show_comment( $comment, $project_id, $class );
+
+            $count++;
         }
     }
     ?>
 </ul>
-<?php cpm_comment_form( $project_id, $task_id ); ?>
+<?php echo cpm_comment_form( $project_id, $task_id ); ?>
