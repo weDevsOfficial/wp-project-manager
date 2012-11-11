390e58a81779edb0217678f6d1beecbb2b4cc8b9
Tareq Hasan <tareq1988@gmail.com>
2012-11-12 04:43:20 +0600

    comment logger functions added. task hook names fix

--- class/logger.php
+++ class/logger.php
@@ -17,11 +17,15 @@ class CPM_Logger {
         add_action( 'cpm_tasklist_update', array($this, 'tasklist_update') );
         add_action( 'cpm_tasklist_delete', array($this, 'tasklist_delete') );
 
-        add_action( 'cpm_new_task', array($this, 'task_new'), 10, 2 );
-        add_action( 'cpm_update_task', array($this, 'task_update'), 10, 2 );
+        add_action( 'cpm_task_new', array($this, 'task_new'), 10, 2 );
+        add_action( 'cpm_task_update', array($this, 'task_update'), 10, 2 );
         add_action( 'cpm_task_complete', array($this, 'task_done') );
         add_action( 'cpm_task_open', array($this, 'task_undo') );
         add_action( 'cpm_task_delete', array($this, 'task_delete') );
+
+        add_action( 'cpm_comment_new', array($this, 'comment_new') );
+        add_action( 'cpm_comment_update', array($this, 'comment_update') );
+        add_action( 'cpm_comment_delete', array($this, 'comment_delete') );
     }
 
     function tasklist_new( $list_id ) {
@@ -53,7 +57,7 @@ class CPM_Logger {
         $tasklist = $this->task_obj->get_task_list( $list_id );
         $task = $this->task_obj->get_task( $task_id );
 
-        $this->log( "task '{$task->post_title}' updated on list {$tasklist->post_title}", 'task' );
+        $this->log( "task '{$task->post_title}' updated on list '{$tasklist->post_title}'", 'task' );
     }
 
     function task_done( $task_id ) {
@@ -61,14 +65,14 @@ class CPM_Logger {
         $tasklist = $this->task_obj->get_task_list( $task->post_parent );
         $user = get_user_by( 'id', get_current_user_id() );
 
-        $this->log( "task '{$task->post_title}' marked as done by '{$user->display_name}' on list '{$tasklist->post_title}'", 'task' );
+        $this->log( "task '{$task->post_title}' marked as completed by '{$user->display_name}' on list '{$tasklist->post_title}'", 'task' );
     }
 
     function task_undo( $task_id ) {
         $task = $this->task_obj->get_task( $task_id );
         $tasklist = $this->task_obj->get_task_list( $task->post_parent );
 
-        $this->log( "task '{$task->post_title}' undo on list '{$tasklist->post_title}'", 'task' );
+        $this->log( "task '{$task->post_title}' re-opened on list '{$tasklist->post_title}'", 'task' );
     }
 
     function task_delete( $task_id ) {
@@ -76,6 +80,27 @@ class CPM_Logger {
         $tasklist = $this->task_obj->get_task_list( $task->post_parent );
 
         $this->log( "task '{$task->post_title}' deleted on list '{$tasklist->post_title}'", 'task' );
+    }
+
+    function comment_new( $comment_id ) {
+        $comment_obj = CPM_Comment::getInstance()->get( $comment_id );
+        $post = get_post( $comment_obj->comment_post_ID );
+
+        $this->log( "new comment added on {$post->post_type} '{$post->post_title}'", 'comment' );
+    }
+
+    function comment_update( $comment_id ) {
+        $comment_obj = CPM_Comment::getInstance()->get( $comment_id );
+        $post = get_post( $comment_obj->comment_post_ID );
+
+        $this->log( "comment#$comment_id updated on {$post->post_type} '{$post->post_title}'", 'comment' );
+    }
+
+    function comment_delete( $comment_id ) {
+        $comment_obj = CPM_Comment::getInstance()->get( $comment_id );
+        $post = get_post( $comment_obj->comment_post_ID );
+
+        $this->log( "comment deleted on {$post->post_type} '{$post->post_title}'", 'comment' );
     }
 
     function log( $message, $type = '' ) {
