a738785a1264b7dbc67411906cdfdd3f631ea3c4
Tareq Hasan <tareq1988@gmail.com>
2012-11-17 04:18:48 +0600

    new message form and delete buttons on message index

--- admin/views/message/index.php
+++ admin/views/message/index.php
@@ -6,7 +6,6 @@
  * TODO: fix search and bulk actions
  * TODO: fix columns
  */
-
 $cpm_active_menu = __( 'Messages', 'cpm' );
 
 $post_type = 'message';
@@ -20,83 +19,40 @@ include CPM_PLUGIN_PATH . '/admin/views/project/header.php';
     Messages <a class="add-new-h2 cpm-new-message-btn" href="<?php echo cpm_url_new_message( $project_id ) ?>">Add New</a>
 </h3>
 
-<form class="cpm-new-message cpm-form" style="display: none">
+<div class="cpm-new-message-form">
+    <h3><?php _e( 'Create a new message', 'cpm' ); ?></h3>
 
-    <?php wp_nonce_field( 'cpm_message' ); ?>
-
-    <table class="form-table">
-        <tbody>
-            
-            <tr class="form-field form-required">
-                <th scope="row">
-                    <label for="message_title">Title <span class="required">*</span></label>
-                </th>
-                <td>
-                    <input name="message_title" type="text" id="message_title" value="" class="required">
-                </td>
-            </tr>
-
-            <tr class="form-field">
-                <th scope="row"><label for="message_detail">Message <span class="required">*</span></label></th>
-                <td><textarea name="message_detail" class="required" id="message_detail" cols="30" rows="10"></textarea></td>
-            </tr>
-
-            <tr class="form-field">
-                <th scope="row"><label for="milestone">Milestone</label></th>
-                <td>
-                    <select name="milestone" id="milestone">
-                        <option value="0"><?php _e( '-- None --', 'cpm' ) ?></option>
-                        <?php echo $milestone_obj->get_dropdown( $project_id ); ?>
-                    </select>
-
-                </td>
-            </tr>
-
-            <tr class="form-field">
-                <th scope="row"><label for="cpm-attachment">Attachment</label></th>
-                <td>
-                    <?php cpm_upload_field(0); ?>
-                </td>
-            </tr>
-
-            <tr class="form-field">
-                <th scope="row"><label for="cpm-notify">Notify</label></th>
-                <td>
-                    <?php cpm_user_checkboxes( $project_id ); ?>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-
-    <p class="submit">
-        <input type="hidden" name="action" value="cpm_new_message" />
-        <input type="hidden" name="project_id" value="<?php echo $project_id; ?>" />
-        <input type="submit" name="create_message" id="create_message" class="button-primary" value="Add Message">
-    </p>
-</form>
+    <?php echo cpm_message_form( $project_id ); ?>
+</div>
 
 <table class="cpm-messages-table">
-<?php
-$messages = $msg_obj->get_all( $project_id );
-foreach ($messages as $message) {
-    ?>
-    <tr>
-        <td class="author">
-            <span class="cpm-avatar"><?php echo cpm_url_user( $message->post_author, true, 32 ); ?></span>
-        </td>
-        <td class="message">
-            <a href="<?php echo cpm_url_single_message( $project_id, $message->ID ); ?>">
-                <span class="title"><?php echo cpm_excerpt( $message->post_title, 50 ); ?></span>
-            <?php if ( $message->post_content ) {
-                printf( '<span class="excerpt"> - %s</span>', cpm_excerpt( $message->post_content, 100) );
-            } ?>
-            </a>
-        </td>
-        <td class="date"><span><?php echo date_i18n( 'j M, Y', strtotime( $message->post_date) ); ?></span></td>
-        <td class="comment-count"><span><?php echo cpm_get_number( $message->comment_count ); ?></span></td>
-    </tr>
     <?php
-}
-?>
+    $messages = $msg_obj->get_all( $project_id );
+    foreach ($messages as $message) {
+        ?>
+        <tr>
+            <td class="author">
+                <span class="cpm-avatar"><?php echo cpm_url_user( $message->post_author, true, 32 ); ?></span>
+            </td>
+            <td class="message">
+                <a href="<?php echo cpm_url_single_message( $project_id, $message->ID ); ?>">
+                    <span class="title"><?php echo cpm_excerpt( $message->post_title, 50 ); ?></span>
+                    <?php
+                    if ( $message->post_content ) {
+                        printf( '<span class="excerpt"> - %s</span>', cpm_excerpt( $message->post_content, 100 ) );
+                    }
+                    ?>
+                </a>
+            </td>
+            <td class="date"><span><?php echo date_i18n( 'j M, Y', strtotime( $message->post_date ) ); ?></span></td>
+            <td class="comment-count"><span><?php echo cpm_get_number( $message->comment_count ); ?></span></td>
+            <td class="cpm-actions">
+                <a href="#" data-msg_id="<?php echo $message->ID; ?>" data-project_id="<?php echo $project_id; ?>" data-confirm="<?php esc_attr_e( 'Are you sure to delete this message?', 'cpm' ); ?>" class="delete-message">
+                    <?php _e( 'Delete', 'cpm' ); ?>
+                </a>
+            </td>
+        </tr>
+        <?php
+    }
+    ?>
 </table>
