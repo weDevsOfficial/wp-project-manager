719d9f5c272719eb8d7cf7a3bdd966ff26acc5bf
Tareq Hasan <tareq1988@gmail.com>
2012-11-20 22:22:34 +0600

    old header codes removed

--- views/project/single.php
+++ views/project/single.php
@@ -1,28 +1,10 @@
 <?php
 /**
  * Project dashboard page
- *
  */
 
-$project = CPM_Project::getInstance();
-$project_detail = $project->get( $project_id );
-
-if ( !$project_detail ) {
-    echo '<h2>' . __( 'Error: Project not found', 'cpm' ) . '</h2>';
-    return;
-}
-
-$cpm_active_menu = __( 'Details', 'cpm' );
+cpm_get_header( __( 'Details', 'cpm' ), $project_id );
 ?>
-<div id="icon-edit" class="icon32 icon32-posts-post"><br></div>
-<h2>
-    <?php _e( 'Project', 'cpm' ) ?> : <?php echo get_the_title( $project_id ); ?>
-    <a href="<?php echo cpm_url_projects(); ?>" class="add-new-h2"><?php _e( 'All Projects', 'cpm' ) ?></a>
-</h2>
-
-<h2 class="nav-tab-wrapper">
-    <?php echo $project->nav_menu( $project_id, $cpm_active_menu ); ?>
-</h2>
 <h3 class="cpm-nav-title">Project Details</h3>
 
 <h1>Project activities will be placed here</h1>
