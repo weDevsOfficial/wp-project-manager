40c7888a919e059e9632eb2ac1ff21f50de553e0
Tareq Hasan <tareq1988@gmail.com>
2012-11-20 22:21:48 +0600

    project detail and edit form added on project header

--- views/project/header.php
+++ views/project/header.php
@@ -1,17 +1,29 @@
 <?php
-$project = CPM_Project::getInstance();
-$project_detail = $project->get( $project_id );
-if ( !$project_detail ) {
+$pro_obj = CPM_Project::getInstance();
+$project = $pro_obj->get( $project_id );
+
+if ( !$project ) {
     echo '<h2>' . __( 'Error: Project not found', 'cpm' ) . '</h2>';
     return;
 }
 ?>
-<div id="icon-edit" class="icon32 icon32-posts-post"><br></div>
-<h2>
-    <?php _e( 'Project', 'cpm' ) ?> : <?php echo get_the_title( $project_id ); ?>
-    <a href="<?php echo cpm_url_projects(); ?>" class="add-new-h2"><?php _e( 'All Projects', 'cpm' ) ?></a>
-</h2>
+<div class="cpm-project-head">
+    <div class="cpm-project-detail">
+        <h2>
+            <span><?php echo get_the_title( $project_id ); ?></span>
+            <a href="#" class="cpm-icon-edit cpm-project-edit-link"><span><?php _e( 'Edit', 'cpm' ); ?></span></a>
+        </h2>
+
+        <div class="detail">
+            <?php echo cpm_get_content( $project->post_content ); ?>
+        </div>
+    </div>
+
+    <div class="cpm-edit-project">
+        <?php cpm_project_form( $project ); ?>
+    </div>
+</div>
 
 <h2 class="nav-tab-wrapper">
-    <?php echo $project->nav_menu( $project_id, $cpm_active_menu ); ?>
+    <?php echo $pro_obj->nav_menu( $project_id, $cpm_active_menu ); ?>
 </h2>
