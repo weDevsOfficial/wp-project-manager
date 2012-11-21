70b052e291a5d0f40382207bb199c9fd360fd14b
Tareq Hasan <tareq1988@gmail.com>
2012-11-22 03:10:17 +0600

    user checkbox bug fix

--- includes/functions.php
+++ includes/functions.php
@@ -69,7 +69,7 @@ function cpm_user_checkboxes( $project_id ) {
     if ( $users ) {
         //var_dump( $users );
         foreach ($users as $user) {
-            $check = sprintf( '<input type="checkbox" name="notify_user[]" id="cpm_notify_%1$d" value="%1$d" />', $user['id'] );
+            $check = sprintf( '<input type="checkbox" name="notify_user[]" id="cpm_notify_%1$s" value="%1$s" />', $user['id'] );
             printf( '<label for="cpm_notify_%d">%s %s</label> ', $user['id'], $check, $user['name'] );
         }
     } else {
