2079eb13702b4ddd4a02ff31307c2c0ae2d2e5be
Tareq Hasan <tareq1988@gmail.com>
2012-11-22 02:18:03 +0600

    project activity added on project single

--- views/project/single.php
+++ views/project/single.php
@@ -2,9 +2,32 @@
 /**
  * Project dashboard page
  */
-
-cpm_get_header( __( 'Details', 'cpm' ), $project_id );
+cpm_get_header( __( 'Activity', 'cpm' ), $project_id );
 ?>
-<h3 class="cpm-nav-title">Project Details</h3>
+<h3 class="cpm-nav-title">
+    <?php _e( 'Project Activity', 'wedevs' ); ?>
 
-<h1>Project activities will be placed here</h1>
+    <?php if ( current_user_can( 'delete_others_posts' ) ) { //editor ?>
+        <span class="cpm-right">
+            <a href="#" class="cpm-icon-delete cpm-project-delete-link" title="<?php esc_attr_e( 'Delete project', 'cpm' ); ?>" <?php cpm_data_attr( array('confirm' => 'Are you sure to delete this project', 'project_id' => $project_id) ) ?>>
+                <span><?php _e( 'Delete', 'cpm' ); ?></span>
+            </a>
+        </span>
+    <?php } ?>
+</h3>
+
+<ul class="cpm-activity dash">
+    <?php
+    $activities = CPM_Comment::getInstance()->get_comments( $project_id, 'DESC' );
+
+    if ( $activities ) {
+        foreach ($activities as $activity) {
+            ?>
+            <li>
+                <?php echo do_shortcode( $activity->comment_content ); ?> <span class="date">- <?php echo cpm_get_date( $activity->comment_date, true ); ?></span>
+            </li>
+            <?
+        }
+    }
+    ?>
+</ul>
