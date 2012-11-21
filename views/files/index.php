8ba1d8f4c075fcd89e88ce2144f2429583db08cb
Tareq Hasan <tareq1988@gmail.com>
2012-11-22 02:27:50 +0600

    attachments show improved

--- views/files/index.php
+++ views/files/index.php
@@ -1,12 +1,11 @@
 <?php
-
 cpm_get_header( __( 'Files', 'cpm' ), $project_id );
 
 $args = array(
     'post_type' => 'attachment',
     'meta_key' => '_project',
     'meta_value' => $project_id,
-    'numberposts' => -1
+    'numberposts' => -1,
 );
 
 $attachments = get_posts( $args );
@@ -15,18 +14,28 @@ if ( $attachments ) {
 
     echo '<ul class="cpm-files">';
     foreach ($attachments as $attachment) {
+
         $file = CPM_Comment::getInstance()->get_file( $attachment->ID );
-        $parent = get_post( $attachment->post_parent );
+        $topic_url = '#';
+
+        if ( !$attachment->post_parent ) {
+            $parent_id = get_post_meta( $attachment->ID, '_parent', true );
+            $parent = get_post( $parent_id );
+        } else {
+            $parent = get_post( $attachment->post_parent );
+        }
+
         $post_type_object = get_post_type_object( $parent->post_type );
 
         if ( $parent->post_type == 'task' ) {
             $task_list = get_post( $parent->post_parent );
             $topic_url = cpm_url_single_task( $project_id, $task_list->ID, $parent->ID );
-        } else if( $parent->post_type == 'message' ) {
+        } else if ( $parent->post_type == 'task_list' ) {
+            $topic_url = cpm_url_single_tasklist( $project_id, $parent->ID );
+        } else if ( $parent->post_type == 'message' ) {
             $topic_url = cpm_url_single_message( $project_id, $parent->ID );
         }
-        // var_dump( $file, $parent );
-        ?>        
+        ?>
         <li>
             <div class="cpm-thumb">
                 <a href="<?php echo $file['url']; ?>"><img src="<?php echo $file['thumb']; ?>" alt="<?php echo esc_attr( $file['name'] ); ?>" /></a>
@@ -35,14 +44,14 @@ if ( $attachments ) {
                 <h3 class="cpm-file-name"><?php echo $file['name']; ?></h3>
 
                 <div class="cpm-file-meta">
-                    Attached to the <a href="<?php echo $topic_url; ?>">comment</a> by <?php echo cpm_url_user( $attachment->post_author ); ?>
+                    <?php printf( __( 'Attached to <a href="%s">%s</a> by %s', 'cpm' ), $topic_url, strtolower( $post_type_object->labels->singular_name ), cpm_url_user( $attachment->post_author ) ) ?>
                 </div>
 
                 <div class="cpm-file-action">
                     <a href="<?php echo $file['url']; ?>">Download</a> or Go to the <a href="<?php echo $topic_url; ?>">discussion</a>
-                    <?php if( $parent->comment_count ) { ?>
+                    <?php if ( $parent->comment_count ) { ?>
                         <span class="comment-number">
-                            <?php printf( _n( '%d comment', '%d comments', get_comments_number( $parent->ID ), 'cpm'), get_comments_number( $parent->ID ) ); ?>
+                            <?php printf( _n( '%d comment', '%d comments', get_comments_number( $parent->ID ), 'cpm' ), get_comments_number( $parent->ID ) ); ?>
                         </span>
                     <?php } ?>
                 </div>
@@ -52,7 +61,6 @@ if ( $attachments ) {
         <?php
     }
     echo '</ul>';
-
 } else {
     cpm_show_message( __( 'No Files Found!', 'cpm' ) );
 }
