e9ba6262227243b36e180806f1359244b54d9ee1
Tareq Hasan <tareq1988@gmail.com>
2013-01-22 20:32:35 +0600

    bug fix: any user could edit a message

--- views/message/single.php
+++ views/message/single.php
@@ -22,12 +22,15 @@ cpm_get_header( __( 'Messages', 'cpm' ), $project_id );
         <span class="cpm-date"><?php echo cpm_url_user( $message->post_author ); ?></span>
         <span class="cpm-separator">|</span>
         <span class="cpm-comment-num"><?php echo cpm_get_number( $message->comment_count ); ?></span>
-        <span class="cpm-separator">|</span>
-        <span class="cpm-edit-link">
-            <a href="#" data-msg_id="<?php echo $message->ID; ?>" data-project_id="<?php echo $project_id; ?>" class="cpm-msg-edit">
-                <?php _e( 'Edit', 'cpm' ); ?>
-            </a>
-        </span>
+        
+        <?php if( $message->post_author == get_current_user_id() ) { ?>
+            <span class="cpm-separator">|</span>
+            <span class="cpm-edit-link">
+                <a href="#" data-msg_id="<?php echo $message->ID; ?>" data-project_id="<?php echo $project_id; ?>" class="cpm-msg-edit">
+                    <?php _e( 'Edit', 'cpm' ); ?>
+                </a>
+            </span>
+        <?php } ?>
     </div>
 
     <div class="cpm-entry-detail">
