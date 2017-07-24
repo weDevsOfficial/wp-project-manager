2f7a5933bc9ea0c4ab0f06d74e46415d36f288b2
pedro-mendonca <ped.gaspar@gmail.com>
2017-07-24 09:31:09 +0100

    Change "threads" to "discussions" for consistency

--- views/js-templates/blanktemplate/files.php
+++ views/js-templates/blanktemplate/files.php
@@ -3,7 +3,7 @@
         <h2 class="cpm-page-title">  <?php _e( 'Documents & Files', 'cpm' ) ?> </h2>
 
         <p>
-            <?php _e( 'Access all the files from all threads in one place. Store your project related files, docs, images or any other files on-site. Makes sharing and storing easy and convenient, plus you can add privacy here too.', 'cpm' ) ?>
+            <?php _e( 'Access all the files from all discussions in one place. Store your project related files, docs, images or any other files on-site. Makes sharing and storing easy and convenient, plus you can add privacy here too.', 'cpm' ) ?>
         </p>
 
 
