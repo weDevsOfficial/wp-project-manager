9a26bd9ebc94ab18bd4c7f246c7e3b1af0dbf949
asikur <md.asikurislam@gmail.com>
2017-08-13 14:20:38 +0600

    routes checked

--- routes/routes.php
+++ routes/routes.php
@@ -2,7 +2,6 @@
 
 use CPM\Core\Router\Router;
 use CPM\Core\Permissions\Administrator;
-
 use CPM\Foo\Validators\Foo_Validator;
 use CPM\Foo\Sanitizers\Foo_Sanitizer;
 
@@ -14,11 +13,15 @@ $router->post( 'foo', 'CPM/Foo/Controllers/Foo_Controller@store' )
     ->permission( [Administrator::class] )
     ->validator( Foo_Validator::class );
 
-$router->get( 'foo/{id}', 'CPM/Foo/Controllers/Foo_Controller@show' );
+$router->get( 'foo/{id}', 'CPM/Foo/Controllers/Foo_Controller@show' )
+    ->permission( [Administrator::class] )
+    ->sanitizer( Foo_Sanitizer::class );
 
 $router->put( 'foo/{id}', 'CPM/Foo/Controllers/Foo_Controller@update' )
     ->permission( [Administrator::class] )
-    ->validator( Foo_Validator::class );
+    ->validator( Foo_Validator::class )
+    ->sanitizer( Foo_Sanitizer::class );
 
 $router->delete( 'foo/{id}', 'CPM/Foo/Controllers/Foo_Controller@destroy' )
-    ->permission( [Administrator::class] );
+    ->permission( [Administrator::class] )
+    ->sanitizer( Foo_Sanitizer::class );
