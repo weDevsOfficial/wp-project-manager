5caea22ea281d3f666fd489b76663740e78c3d86
asikur <md.asikurislam@gmail.com>
2017-08-17 14:54:57 +0600

    Controller for category created

---
+++ src/Category/Controllers/Category_Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CPM\Category\Controllers;
+
+use WP_REST_Request;
+
+class Category_Controller {
+    public function index( WP_REST_Request $request ) {
+
+    }
+
+    public function show( WP_REST_Request $request ) {
+
+    }
+
+    public function store( WP_REST_Request $request ) {
+
+    }
+
+    public function update( WP_REST_Request $request ) {
+
+    }
+
+    public function destroy( WP_REST_Request $request ) {
+
+    }
+}
