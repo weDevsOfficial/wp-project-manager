41ee351f613bf73c50cd31ad2834df84bc6e75b9
asikur <md.asikurislam@gmail.com>
2017-08-20 23:19:52 +0600

    Todo list showing done

--- routes/todolist.php
+++ routes/todolist.php
@@ -7,6 +7,6 @@ $router = Router::singleton();
 
 $router->get( 'projects/{project_id}/todo-lists', 'CPM/Todo_List/Controllers/Todo_List_Controller@index' );
 $router->post( 'projects/{project_id}/todo-lists', 'CPM/Todo_List/Controllers/Todo_List_Controller@store' );
-$router->get( 'projects/{project_id}/todolists/{milestone_id}', 'CPM/Milestone/Controllers/Milestone_Controller@show' );
+$router->get( 'projects/{project_id}/todo-lists/{todo_list_id}', 'CPM/Todo_List/Controllers/Todo_List_Controller@show' );
 $router->put( 'projects/{project_id}/todolists/{milestone_id}', 'CPM/Milestone/Controllers/Milestone_Controller@update' );
 $router->delete( 'projects/{project_id}/todolists/{milestone_id}', 'CPM/Milestone/Controllers/Milestone_Controller@destroy' );
--- src/Todo_List/Controllers/Todo_List_Controller.php
+++ src/Todo_List/Controllers/Todo_List_Controller.php
@@ -25,6 +25,19 @@ class Todo_List_Controller {
         return $this->get_response( $resource );
     }
 
+    public function show( WP_REST_Request $request ) {
+        $project_id = $request->get_param( 'project_id' );
+        $todo_list_id = $request->get_param( 'todo_list_id' );
+
+        $todo_list = Todo_List::where( 'id', $todo_list_id )
+            ->where( 'project_id', $project_id )
+            ->first();
+
+        $resource = new Item( $todo_list, new Todo_List_Transformer );
+
+        return $this->get_response( $resource );
+    }
+
     public function store( WP_REST_Request $request ) {
         $data = [
             'title' => $request->get_param( 'title' ),
