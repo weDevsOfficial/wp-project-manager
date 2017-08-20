dc6e315e39f3f63f518d8cc85c58f078c92c6e58
asikur <md.asikurislam@gmail.com>
2017-08-20 23:36:38 +0600

    Todo list item update operation done

--- src/Todo_List/Controllers/Todo_List_Controller.php
+++ src/Todo_List/Controllers/Todo_List_Controller.php
@@ -53,4 +53,26 @@ class Todo_List_Controller {
 
         return $this->get_response( $resource );
     }
+
+    public function update( WP_REST_Request $request ) {
+        $project_id = $request->get_param( 'project_id' );
+        $todo_list_id = $request->get_param( 'todo_list_id' );
+
+        $todo_list = Todo_List::where( 'id', $todo_list_id )
+            ->where( 'project_id', $project_id )
+            ->first();
+
+        $data = [
+            'title' => $request->get_param( 'title' ),
+            'description' => $request->get_param( 'description' ),
+            'order' => $request->get_param( 'order' ),
+        ];
+        $data = array_filter( $data );
+
+        $todo_list->update( $data );
+
+        $resource = new Item( $todo_list, new Todo_List_Transformer );
+
+        return $this->get_response( $resource );
+    }
 }
