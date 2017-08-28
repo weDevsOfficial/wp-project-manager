7a25c8ddae5efa885142d739cd30e0082ac1dc4a
asikur <md.asikurislam@gmail.com>
2017-08-28 12:32:27 +0600

    Comment controller inititated

---
+++ src/Comment/Controllers/Comment_Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CPM\Comment\Controllers;
+
+use WP_REST_Request;
+
+class Comment_Controller {
+    public function index( WP_REST_Request $request ) {
+
+    }
+
+    public function show( WP_REST_Request $request ) {
+
+    }
+
+    public function store( WP_REST_Request $request ) {
+
+    }
+
+    public function update( WP_REST_Request $request ) {
+
+    }
+
+    public function destroy( WP_REST_Request $request ) {
+
+    }
+}
