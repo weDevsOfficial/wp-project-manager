2e70326c29c10fe3dce4110fdedc8a9ea6945b14
asikur <md.asikurislam@gmail.com>
2017-09-28 10:08:26 +0600

    Creator and updater field included with category

--- src/Category/Transformer/Category_Transformer.php
+++ src/Category/Transformer/Category_Transformer.php
@@ -4,18 +4,32 @@ namespace CPM\Category\Transformer;
 
 use CPM\Category\Models\Category;
 use League\Fractal\TransformerAbstract;
+use CPM\User\Transformers\User_Transformer;
 
-class Category_Transformer extends TransformerAbstract
-{
-    public function transform( Category $item )
-    {
+class Category_Transformer extends TransformerAbstract {
+
+    protected $defaultIncludes = [
+        'creator', 'updater',
+    ];
+
+    public function transform( Category $item ) {
         return [
             'id' => (int) $item->id,
             'title' => $item->title,
             'description' => $item->description,
             'categorible_type' => $item->categorible_type,
-            'created_by' => $item->created_by,
-            'updated_by' => $item->updated_by,
         ];
+    }
+
+    public function includeCreator( Category $item ) {
+        $creator = $item->creator;
+
+        return $this->item( $creator, new User_Transformer );
+    }
+
+    public function includeUpdater ( Category $item ) {
+        $updater = $item->updater;
+
+        return $this->item( $updater, new User_Transformer );
     }
 }
