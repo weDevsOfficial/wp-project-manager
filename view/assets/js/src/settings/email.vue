0321bddaed59463c76fd03d5be6e092075c63578
Asaquzzaman <joy.mishu@gmail.com>
2017-10-09 15:18:49 +0600

    Setting has been completed

--- view/assets/js/src/settings/email.vue
+++ view/assets/js/src/settings/email.vue
@@ -3,7 +3,7 @@
 		<settings-header></settings-header>
 		<div class="metabox-holder">
 			<div id="cpm_mails" class="group" style="">
-				<form method="post" action="options.php">
+				<form @submit.prevent="saveEmailSettings()" method="post" action="options.php">
 			
 					<h2>E-Mail Settings</h2>
 					<table class="form-table">
@@ -13,7 +13,7 @@
 									<label for="cpm_mails[email_from]">From Email</label>
 								</th>
 								<td>
-									<input type="text" class="regular-text" id="cpm_mails[email_from]" name="cpm_mails[email_from]" value="joy.mishu@gmail.com">
+									<input v-model="from_email" type="text" class="regular-text" id="cpm_mails[email_from]" name="cpm_mails[email_from]" value="joy.mishu@gmail.com">
 								</td>
 							</tr>
 							<tr>
@@ -23,7 +23,7 @@
 								<td>
 									<fieldset>
 										<label for="wpuf-cpm_mails[email_url_link][backend]">
-											<input type="radio" class="radio" id="wpuf-cpm_mails[email_url_link][backend]" name="cpm_mails[email_url_link]" value="backend" checked="checked">Link to Backend
+											<input v-model="link_to_backend" type="checkbox" class="radio">Link to Backend
 										</label>
 										<br>
 										<p class="description">Select where do you want to take the user. Notification emails contain links.
@@ -36,7 +36,7 @@
 									<label for="cpm_mails[email_type]">E-Mail Type</label>
 								</th>
 								<td>
-									<select class="regular" name="cpm_mails[email_type]" id="cpm_mails[email_type]">
+									<select v-model="email_type" class="regular" name="cpm_mails[email_type]" id="cpm_mails[email_type]">
 										<option value="text/html">HTML Mail</option>
 										<option value="text/plain" selected="selected">Plain Text</option>
 									</select>
@@ -49,8 +49,8 @@
 								<td>
 									<fieldset>
 										<label for="wpuf-cpm_mails[email_bcc_enable]">
-											<input type="hidden" name="cpm_mails[email_bcc_enable]" value="off">
-											<input type="checkbox" class="checkbox" id="wpuf-cpm_mails[email_bcc_enable]" name="cpm_mails[email_bcc_enable]" value="on">Enable Bcc
+											<input v-model="enable_bcc" type="checkbox" class="checkbox">
+											Enable Bcc
 										</label>
 									</fieldset>
 								</td>
@@ -75,6 +75,31 @@
         components: {
             'settings-header': header
         },
+
+        data () {
+            return {
+                from_email: this.getSettings('from_email', PM_Vars.current_user.data.user_email),
+                link_to_backend: this.getSettings('link_to_backend', false),
+                email_type: this.getSettings('email_type', 'text/html'),
+                enable_bcc: this.getSettings('enable_bcc', false),
+            }
+        },
+
+        methods: {
+            saveEmailSettings () {
+                var data = {
+                    from_email: this.from_email,
+                    link_to_backend: this.link_to_backend,
+                    email_type: this.email_type,
+                    enable_bcc: this.enable_bcc
+                };
+
+
+                this.saveSettings(data, function(res) {
+
+                });
+            },
+        }
     }
 </script>
 
