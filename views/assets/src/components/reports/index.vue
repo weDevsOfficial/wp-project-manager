d686c7dfb498a3d61c79928c21ca28cc7546cfda
mshossain110 <mshossain11@gmail.com>
2017-11-19 08:47:10 +0600

    added progress router

--- views/assets/src/components/reports/index.vue
+++ views/assets/src/components/reports/index.vue
@@ -1,11 +1,5 @@
 <template>
-    <p>Hi i am reports</p>
+    <div>
+        <router-view name="reports"></router-view>
+    </div>
 </template>
-
-<script>
-    export default {
-        methods: {
-
-        }
-    }
-</script>
