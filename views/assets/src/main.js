097db90ff5686296378ee31f0a3893afd144ae26
Asaquzzaman <joy.mishu@gmail.com>
2018-01-25 09:46:21 +0600

    Update dependency for main scripts

--- views/assets/src/main.js
+++ views/assets/src/main.js
@@ -1,21 +1,97 @@
-import store from './store/store';
-import router from './router/router';
-import directive from './directives/directive';
-import mixin from './helpers/mixin/mixin';
-import PM from './App.vue';
-import PMComponents from './helpers/common-components';
+import Promise from 'promise-polyfill'; 
 
-/**
- * Project template render
- */
-var PM_Vue = {
-    el: '#wedevs-pm',
-    store,
-    router,
-    render: t => t(PM),
+var store, router, directive, mixin, PM, PMComponents;
+
+var scriptsLoaded = {
+	'store': false,
+	'router': false,
+	'directive': false,
+	'mixin': false,
+	'PM': false,
+	'PMComponents': false,
+};
+
+var promiseReturn = new Promise(function(resolve, reject) {
+
+
+
+	require(['./router/router'], function(script) {
+		router = script.default;
+		scriptsLoaded.router = true;
+		pmScriptsLoaded(resolve, reject);
+
+
+		weDevsPmModules.forEach(function(module) {
+            let mixin = require('./components/'+module.path+'/mixin.js');
+            PmMixin[module.name] = mixin.default;
+        });
+
+		
+		require(['./store/store'], function(script) {
+			store = script.default;
+			scriptsLoaded.store = true;
+			pmScriptsLoaded(resolve, reject);
+		});
+
+		require(['./directives/directive'], function(script) {
+			console.log(script);
+			directive = script.default;
+			scriptsLoaded.directive = true;
+			pmScriptsLoaded(resolve, reject);
+		});
+
+		require(['./helpers/mixin/mixin'], function(script) {
+			mixin = script.default;
+			scriptsLoaded.mixin = true;
+			pmScriptsLoaded(resolve, reject);
+		});
+
+		require(['./App.vue'], function(script) {
+			PM = script.default;
+			scriptsLoaded.PM = true;
+			pmScriptsLoaded(resolve, reject);
+		});
+
+		require(['./helpers/common-components'], function(script) {
+			PMComponents = script.default;
+			scriptsLoaded.PMComponents = true;
+			pmScriptsLoaded(resolve, reject);
+		});
+	});
+
+});
+
+function pmScriptsLoaded(resolve, reject) {
+	var loaded = Object.values(scriptsLoaded);
+	
+	if (loaded.indexOf(false) === -1) {
+		resolve(true);
+	}
 }
 
-new pm.Vue(PM_Vue); 
+//import store from './store/store';
+// import router from './router/router';
+// import directive from './directives/directive';
+// import mixin from './helpers/mixin/mixin';
+// import PM from './App.vue';
+// import PMComponents from './helpers/common-components';
+
+
+
+promiseReturn.then(function(result) {
+	/**
+	 * Project template render
+	 */
+	var PM_Vue = {
+	    el: '#wedevs-pm',
+	    store,
+	    router,
+	    render: t => t(PM),
+	}
+
+	new pm.Vue(PM_Vue); 
+});
+
 
 
 
