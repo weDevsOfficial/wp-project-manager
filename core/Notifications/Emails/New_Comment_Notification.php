1c66169c97ae87dc9274b0e69332a640067266cb
mshossain110 <mshossain110@gmail.com>
2018-04-02 12:50:21 +0600

    fixed comment notification issues

--- core/Notifications/Emails/New_Comment_Notification.php
+++ core/Notifications/Emails/New_Comment_Notification.php
@@ -7,7 +7,12 @@ namespace WeDevs\PM\Core\Notifications\Emails;
 */
 use WeDevs\PM\Core\Notifications\Email;
 use WeDevs\PM\Project\Models\Project;
+use WeDevs\PM\Task_List\Models\Task_List;
+use WeDevs\PM\Task\Models\Task;
+use WeDevs\PM\Discussion_Board\Models\Discussion_Board;
+use WeDevs\PM\Common\Models\Meta;
 use WeDevs\PM\Comment\Models\Comment;
+use WeDevs\PM\File\Models\File;
 
 class New_Comment_Notification extends Email {
     
@@ -17,48 +22,63 @@ class New_Comment_Notification extends Email {
     }
 
     public function trigger( $commentData, $request ) {
+
         if ( empty( $request['notify_users'] ) ){
             return ;
         }
 
         $project      = Project::with('assignees', 'managers')->find( $request['project_id'] );
-        $comment      = Comment::with($request['commentable_type'])->find($commentData['data']['id']);
         $users        = array();
         $notify_users = explode( ',',  $request['notify_users'] );
 
-        foreach ($notify_users as $u ) {
+        foreach ( $notify_users as $u ) {
             if( $this->is_enable_user_notification( $u ) ){
-                $users[] = $project->assignees->where('ID', $u)->first()->user_email;
+                $users[] = $project->assignees->where( 'ID', $u )->first()->user_email;
             }
         }
 
-        if( apply_filters('notify_project_managers', true) ){
-            foreach ($project->managers as $u) {
-                if(!in_array($u->user_email, $users)){
+        if( apply_filters( 'notify_project_managers', true ) ){
+            foreach ( $project->managers as $u ) {
+                if ( !in_array( $u->user_email, $users ) ) {
                     $users[] = $u->user_email;
                 }
             }
         }
 
-        if( !$users ){
+        if ( !$users ){
             return ; 
         }
-        $title         = $comment->{ $request['commentable_type'] }->title;
-        $template_name = apply_filters( 'pm_new_comment_email_template_path', $this->get_template_path( '/html/new-comment.php' ) );
-        $subject       = sprintf( __( '[%s][%s] New Comment on: %s', 'pm' ), $this->get_blogname(), $project->title , $title );
-        
-        if( $request['commentable_type'] == 'discussion_board' ){
+
+        if ( $request['commentable_type'] == 'discussion_board' ) {
             $type = __( 'Message', 'pm' );
-            $comment_link = $this->pm_link() . '#/'.$project->id.'/discussions/'.$request['commentable_id'];
-        }elseif ( $request['commentable_type'] == 'task_list' ) {
+            $comment_link = $this->pm_link() . '#/projects/'.$project->id.'/discussions/'.$request['commentable_id'];
+            $title = Discussion_Board::find( $request['commentable_id'] )->title;
+
+        } else if ( $request['commentable_type'] == 'task_list' ) {
             $type = __( 'Task List', 'pm' );
-            $comment_link = $this->pm_link() . '#/'.$project->id.'/task-lists/'.$request['commentable_id'];
-        }else{
+            $comment_link = $this->pm_link() . '#/projects/'.$project->id.'/task-lists/'.$request['commentable_id'];
+            $title = Task_List::find( $request['commentable_id'] )->title;
+
+        } else if ( $request['commentable_type'] == 'task' ) {
             $type        = __( 'Task', 'pm' );
-            $comment_link = $this->pm_link() . '#/'.$project->id.'/task/'.$request['commentable_id'];
+            $comment_link = $this->pm_link() . '#/projects/'.$project->id.'/task/'.$request['commentable_id'];
+            $title = Task::find( $request['commentable_id'] )->title;
+
+        } else if ( $request['commentable_type'] == 'file' ) {
+            $type        = __( 'File', 'pm' );
+            $file = File::find($request['commentable_id']);
+            $comment_link = $this->pm_link() . '#/projects/'. $project->id .'/files/'. $file->parent .'/'. $file->type .'/'. $request['commentable_id'];
+            $filemeta = Meta::where( 'project_id', $request['project_id'] )
+                            ->where( 'entity_type', 'file' )
+                            ->where( 'entity_id',  $request['commentable_id'])
+                            ->where( 'meta_key', 'title' )
+                            ->first();
+            $title = $filemeta->meta_value;
+
         }
 
-       
+        $template_name = apply_filters( 'pm_new_comment_email_template_path', $this->get_template_path( '/html/new-comment.php' ) );
+        $subject       = sprintf( __( '[%s][%s] New Comment on: %s', 'pm' ), $this->get_blogname(), $project->title , $title );       
         
         $message = $this->get_content_html( $template_name, [
             'id'                => $commentData['data']['id'],
@@ -70,7 +90,6 @@ class New_Comment_Notification extends Email {
         ] );
 
         $this->send( $users, $subject, $message );
-
     }
 
 }
