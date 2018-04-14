4bbdeb2ae57e6542743729e2417bf930266e1223
Asaquzzaman <joy.mishu@gmail.com>
2018-04-14 10:25:43 +0600

    Beta version 2.0

--- config/app.php
+++ config/app.php
@@ -4,6 +4,6 @@ return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
     'version'     => '2',
-    'db_version'  => '2.0',
+    'db_version'  => '2.0-beta',
     'text_domain' => 'pm'
 ];
