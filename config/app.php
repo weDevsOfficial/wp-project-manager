069a05575dcc5a363344e3ca3114263c7c9efa9a
mshossain110 <mshossain110@gmail.com>
2018-06-23 15:56:42 +0600

    added indexd db and foreign key references

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2',
+    'version'     => '2.0.0',
     'db_version'  => '2.0-beta',
     'text_domain' => 'pm'
 ];
