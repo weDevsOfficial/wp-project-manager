322d07f716cad78c17f1ee0937f7f2d7c0b4593e
mshossain110 <mshossain110@gmail.com>
2018-07-20 14:20:00 +0600

    fix: version compare

--- config/app.php
+++ config/app.php
@@ -6,6 +6,5 @@ return [
     'version'     => '2.0.1',
     'api'     	  => '2',
     'db_version'  => '2.0',
-    'text_domain' => 'pm',
-    'download_link' => 'https://github.com/weDevsOfficial/wp-project-manager/releases/download/v2.0.1/wedevs-project-manager-php5.6-v2.0.1.zip',
+    'text_domain' => 'pm'
 ];
