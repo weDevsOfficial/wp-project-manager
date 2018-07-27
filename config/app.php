8e1a6a1d6603c52c40dd55857540e43fdcf1574c
Asaquzzaman <joy.mishu@gmail.com>
2018-07-27 11:37:29 +0600

    Release: Version 2.0.3

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.0.2',
+    'version'     => '2.0.3',
     'api'     	  => '2',
     'db_version'  => '2.0',
     'text_domain' => 'pm'
