d3cf9758268850c27b0557ff0dafb47c6c0736c5
Asaquzzaman <joy.mishu@gmail.com>
2018-08-16 09:26:12 +0600

    Feature: Advance task list filter

--- src/Task_List/Transformers/Task_List_Transformer.php
+++ src/Task_List/Transformers/Task_List_Transformer.php
@@ -14,6 +14,8 @@ use WeDevs\PM\Common\Traits\Resource_Editors;
 use WeDevs\PM\Task\Models\Task;
 use Illuminate\Database\Capsule\Manager as DB;
 use Illuminate\Pagination\Paginator;
+use League\Fractal\Resource\Collection as Collection;
+
 
 class Task_List_Transformer extends TransformerAbstract {
 
