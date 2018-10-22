f32e90999142161ff484e7db6a4bb9c78a4cf2b2
mshossain110 <mshossain110@gmail.com>
2018-10-22 14:26:27 +0600

    archived task list with new design

--- views/assets/js/pmglobal.js
+++ views/assets/js/pmglobal.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 494);
+/******/ 	return __webpack_require__(__webpack_require__.s = 497);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 494:
+/***/ 497:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
