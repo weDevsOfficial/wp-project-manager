f65780acba005281c079118b2c5e3fe2fdcb7a04
mshossain110 <mshossain110@gmail.com>
2018-11-08 10:02:41 +0600

    task creating reature script

---
+++ views/project-switch/task-creating.php
@@ -0,0 +1,15 @@
+<div class="pm-cteate-task pm-global" id="pmcteatetask">
+    <div class="inner">
+        
+        <form class="new-task-form"  id="newtaskform">
+            <div >
+                <input id="task-title" type="text" placeholder="Task title">
+                
+            </div>
+            <div class="select-project"></div>
+        </form>
+        
+    </div>
+    <div class="pmbackoverlay" ></div>
+    
+</div>
