7c5be8286faae99483c0e79bf02386bebf545b8e
almas_alright <kutsnalmas@gmail.com>
2019-01-20 12:40:14 +0600

    active collab auth

---
+++ src/Tools/Controllers/ActivecolController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: faizuralmas
+ * Date: 20/1/19
+ * Time: 12:14 PM
+ */
+
+namespace WeDevs\PM\Tools\Controllers;
+
+use WP_REST_Request;
+
+class ActivecolController
+{
+    public function authAc(WP_REST_Request $request){
+        return $request;
+    }
+
+}
