ea125d5ac3932f163203167e9deaa4a0043c37e7
almas-alright <kutsnalmas@gmail.com>
2019-01-21 09:31:13 +0600

    active collab token problem

--- src/Tools/Controllers/ActivecolController.php
+++ src/Tools/Controllers/ActivecolController.php
@@ -8,12 +8,48 @@
 
 namespace WeDevs\PM\Tools\Controllers;
 
+use ActiveCollab\SDK\Authenticator\Cloud;
 use WP_REST_Request;
 
 class ActivecolController
 {
     public function authAc(WP_REST_Request $request){
-        return $request;
+        $username = $request->get_param('user');
+        $password = $request->get_param('pass');
+        $activeColAuth = new Cloud(
+            'weDevs LLC',
+            'WPPM',
+            $username,
+            $password
+            );
+
+        return $activeColAuth->getAccounts();
     }
+
+    public function tokenAc(WP_REST_Request $request){
+
+        $username = $request->get_param('user');
+        $password = $request->get_param('pass');
+        $acount = $request->get_param('accid');
+        $activeColAuth = new Cloud(
+            'weDevs LLC',
+            'WPPM',
+            $username,
+            $password
+        );
+        $activeColAuth->getAccounts();
+        $token = $activeColAuth->issueToken($acount);
+
+        if ($token instanceof \ActiveCollab\SDK\TokenInterface) {
+            print $token->getUrl() . "\n";
+            print $token->getToken() . "\n";
+        } else {
+            print "Invalid response\n";
+            die();
+        }
+//       return $acount.$username.$password;
+    }
+
+
 
 }
---
+++ src/Tools/Library/PM_ActiveCol.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wedevs-macbook-2
+ * Date: 20/1/19
+ * Time: 9:25 PM
+ */
+
+namespace WeDevs\PM\Tools\Library;
+
+
+class PM_ActiveCol
+{
+
+}
