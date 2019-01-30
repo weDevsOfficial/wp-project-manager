93b68611b1564d9757e69b297b61b84f0cc4a3b7
Mehidi Hassan <mehidihassan258@gmail.com>
2019-01-30 16:08:16 +0600

    added import section and disabled youtube suggested videos

--- views/assets/src/components/welcome/featureWidget.vue
+++ views/assets/src/components/welcome/featureWidget.vue
@@ -3,7 +3,7 @@
         
         <div class="pm-video-modal">
             <pm-modal :is-active="show" @close="show=false" :width="600">
-                <iframe width="600" height="315" :src="'https://www.youtube.com/embed/' + feature.featureVideo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="600" height="315" :src="'https://www.youtube.com/embed/' + feature.featureVideo + '/?rel=0'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </pm-modal>
         </div>
         <div :class="['pm-feature-card', feature.cardClass]">
