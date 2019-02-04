10c1318ddaaea537a6189b3267a167bdc80bbb7a
almas-alright <kutsnalmas@gmail.com>
2019-02-04 10:33:40 +0600

    correction : router

--- views/assets/src/router/router.js
+++ views/assets/src/router/router.js
@@ -2,28 +2,29 @@
 import projects from '@components/project-lists/router';
 import categories from '@components/categories/router';
 import add_ons from '@components/add-ons/router';
-import '@components/tools/router';
 
 if (!PM_Vars.is_pro) {
     require( '@components/my-tasks/router');
     require('@components/calendar/router');
     require('@components/reports/router');
     require('@components/progress/router');
+    require('@components/welcome/router');
+    require('@components/pro-features/router');
 }
 
 import {general, email} from '@components/settings/router';
 import Empty from '@components/root/init.vue';
 
 weDevs_PM_Routers.push({
-	path: '/',
+    path: '/',
     component:  Empty,
     name: 'project_root',
 
-	children: wedevsPMGetRegisterChildrenRoute('project_root')
+    children: wedevsPMGetRegisterChildrenRoute('project_root')
 });
 
 var router = new pm.VueRouter({
-	routes: weDevs_PM_Routers,
+    routes: weDevs_PM_Routers,
 });
 
 router.beforeEach((to, from, next) => {
@@ -40,4 +41,3 @@ weDevsPmModules.forEach(function(module) {
 
 
 export default router;
-
