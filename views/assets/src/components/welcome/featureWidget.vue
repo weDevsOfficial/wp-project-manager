df0d38a63a1295fa3b9d1812f7503a42508955f1
Asaquzzaman <joy.mishu@gmail.com>
2019-02-11 11:46:42 +0600

    Change: Modal file for welcome page

--- views/assets/src/components/welcome/featureWidget.vue
+++ views/assets/src/components/welcome/featureWidget.vue
@@ -2,9 +2,9 @@
     <div class="pm-col-sm-4 pm-col-xs-12">
         
         <div class="pm-video-modal">
-            <pm-modal v-if="show" :is-active="show" @close="show=false" :width="600">
+            <welcome-modal v-if="show" :is-active="show" @close="show=false" :width="600">
                 <iframe width="600" height="315" :src="'https://www.youtube.com/embed/' + feature.featureVideo + '/?VQ=HD1080&autoplay=1&rel=0'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </pm-modal>
+            </welcome-modal>
         </div>
         <div :class="['pm-feature-card', feature.cardClass]">
             <div class="pm-feature-thumb">
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-    import pmModal from '@components/common/Modal.vue';
+    import WelcomeModal from './modal.vue';
     export default {
         data(){
             return {
@@ -34,7 +34,7 @@
             }
         },
         components: {
-            pmModal
+            'welcome-modal': WelcomeModal
         }
     }
 </script>
