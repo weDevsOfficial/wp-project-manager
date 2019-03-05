ef9f281bc157dc040be0d35c0b318150dfbf706d
Asaquzzaman <joy.mishu@gmail.com>
2019-03-05 12:14:44 +0600

    Fix: Authentication problem

--- core/Permissions/Access_Project.php
+++ core/Permissions/Access_Project.php
@@ -7,15 +7,17 @@ use WP_REST_Request;
 
 class Access_Project extends Abstract_Permission {
    
-    public function check() {
+    public function check() { 
         $project_id = $this->request->get_param( 'project_id' );
 
         if ( empty($project_id) ){
             $project_id = $this->request->get_param( 'id' );
         }
-        $user_id = $this->request->get_param( 'user_id' ); 
-        
-        if ( pm_user_can( 'view_project', $project_id, $user_id ) ){
+
+        // $user_id = $this->request->get_param( 'user_id' ); 
+        // $user_id = empty( $user_id ) ? 0 : intval( $user_id );
+
+        if ( pm_user_can( 'view_project', $project_id ) ) {
             return true;
         }
 
