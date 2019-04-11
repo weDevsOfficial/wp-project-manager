ba2b365ff8a7680a8f7f3834a0a6ba9327d1e99d
Asaquzzaman <joy.mishu@gmail.com>
2019-04-11 09:54:33 +0600

    Deploying v2.0.14

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.0.13',
+    'version'     => '2.0.14',
     'api'     	  => '2',
     'db_version'  => '2.2.1',
     'text_domain' => 'pm',
