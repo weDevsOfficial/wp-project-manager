e02520cf87e42b8aa0e4a95581c07d81d0b8c482
opurockey <opu.rockey@gmail.com>
2019-04-30 10:19:53 +0600

    Deleted unnecessary components

--- views/assets/src/router/router.js
+++ views/assets/src/router/router.js
@@ -1,7 +1,6 @@
 import projects from '@components/project-lists/router';
 import categories from '@components/categories/router';
 import add_ons from '@components/add-ons/router';
-import '@components/importtools/router';
 //import '@components/tools/router';
 
 if (!PM_Vars.is_pro) {
