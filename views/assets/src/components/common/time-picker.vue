ea1953c042c961790b6ea79ce5712ce67727a1b8
opurockey <opu.rockey@gmail.com>
2019-05-03 10:25:16 +0600

    Added custom time tracker validation

--- views/assets/src/components/common/time-picker.vue
+++ views/assets/src/components/common/time-picker.vue
@@ -1,14 +1,24 @@
 <template>
-    <input type="text" :value="value">
+    <input type="text" :value="value" :placeholder="placeholderValue">
 </template>
 
 <script>
     export default {
         props: ['value', 'dependency','field_type'],
+        data(){
+           return {
+               placeholderValue : ''
+           }
+        },
         mounted: function() {
             var self = this,
                 limit_date = ( self.dependency == 'pm-datepickter-from' ) ? "maxDate" : "minDate",
-                cur_field_type = (self.field_type == 'date') ? false : true ;;
+                cur_field_type = (self.field_type == 'date') ? false : true ;
+                if(self.field_type == 'date'){
+                   self.placeholderValue = 'Ex : 2019-12-31'
+                }else{
+                    self.placeholderValue = 'Ex : 01:30'
+                }
 
             var dateObj = {
                 dateFormat: 'yy-mm-dd',
