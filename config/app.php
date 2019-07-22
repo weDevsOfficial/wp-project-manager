025cd176e0002624d546661e9e700032eab06d79
Asaquzzaman <joy.mishu@gmail.com>
2019-07-22 10:23:20 +0600

    v2.2.4

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.2.3',
+    'version'     => '2.2.4',
     'api'     	  => '2',
     'db_version'  => '2.2.2',
     'text_domain' => 'pm',
