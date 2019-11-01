33098c81871864f3ba64c37e008740ae7187422b
Asaquzzaman <joy.mishu@gmail.com>
2019-11-01 12:22:22 +0600

    Remove pusher route

--- config/define.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-	'path'       => dirname( dirname( __FILE__ ) ),
-	'url'        => plugin_dir_url( dirname( __FILE__ ) ),
-    'view_path'  => dirname( dirname( __FILE__ ) ) . '/views',
-];
-       
