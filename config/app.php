a5cc4806e23ac4ae6d37f14c110a6347bedad1ad
Asaquzzaman <joy.mishu@gmail.com>
2020-02-10 10:51:39 +0600

    v.2.3.6

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.3.5',
+    'version'     => '2.3.6',
     'api'     	  => '2',
     'db_version'  => '2.3',
     'text_domain' => 'pm',
