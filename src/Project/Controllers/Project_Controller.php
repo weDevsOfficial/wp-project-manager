fe67a2be996e34a22ff24607c9abb69f16a88be4
Asaquzzaman <joy.mishu@gmail.com>
2020-02-27 09:43:49 +0600

    Fix coding indentation

--- src/Project/Controllers/Project_Controller.php
+++ src/Project/Controllers/Project_Controller.php
@@ -235,6 +235,7 @@ class Project_Controller {
 		$resource = new Item( $project, new Project_Transformer );
 		$response = $this->get_response( $resource );
 		$response['message'] = pm_get_text('success_messages.project_created');
+		
 		do_action( 'cpm_project_new', $project->id, $project->toArray(), $request->get_params() ); // will deprecated
 		do_action( 'pm_after_new_project', $response, $request->get_params() );
 
