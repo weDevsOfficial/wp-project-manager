8a1cc3099af63ec30a0cc7bce9172c2c431f61e1
Asaquzzaman <joy.mishu@gmail.com>
2020-04-02 12:54:31 +0600

    Task label getting error when there is no task

--- src/Task/Controllers/Task_Controller.php
+++ src/Task/Controllers/Task_Controller.php
@@ -1059,13 +1059,19 @@ class Task_Controller {
     public function get_tasks( $task_ids, $args=[] ) {
         global $wpdb;
 
+        foreach ( (array) $task_ids as $key => $task_id ) {
+            if ( empty( intval( $task_id ) ) ) {
+                unset( $task_ids[$key] );
+            }
+        }
+
         $default = [
             'list_task_transormer_filter' => true
         ];
 
         $args = wp_parse_args( $args, $default );
 
-        if ( empty( $list_ids ) ) {
+        if ( empty( $task_ids ) ) {
             $task_ids[] = 0;
         }
 
