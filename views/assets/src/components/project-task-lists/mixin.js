5b22b5da58719e6365b00bd960ae74122b7f8374
Asaquzzaman <joy.mishu@gmail.com>
2020-06-02 11:00:25 +0600

    As a co-worker, can not edit task list from task lists page which list he/she created closed #361

--- views/assets/src/components/project-task-lists/mixin.js
+++ views/assets/src/components/project-task-lists/mixin.js
@@ -82,8 +82,10 @@ var PM_TaskList_Mixin = {
             if (this.is_manager()) {
                 return true;
             }
-
-            if ( list.creator.data.id == user.ID ){
+            
+            let creatorId = list.creator.data.id ? list.creator.data.id : list.creator.data.ID;
+            
+            if ( creatorId == user.ID ){
                 return true;
             }
 
