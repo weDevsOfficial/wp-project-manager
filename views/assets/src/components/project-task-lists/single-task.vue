bd6332e71a7870ec198611305fa3faae298fbfbe
Asaquzzaman <joy.mishu@gmail.com>
2020-06-23 15:14:06 +0600

    send click wrap event

--- views/assets/src/components/project-task-lists/single-task.vue
+++ views/assets/src/components/project-task-lists/single-task.vue
@@ -131,7 +131,7 @@
                                             @click.prevent="deleteUser(user)"
                                         >
 
-                                            <span class="cross"><i><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 241.171 241.171" style="enable-background:new 0 0 241.171 241.171;" xml:space="preserve"><path id="Close" d="M138.138,120.754l99.118-98.576c4.752-4.704,4.752-12.319,0-17.011c-4.74-4.704-12.439-4.704-17.179,0 l-99.033,98.492L21.095,3.699c-4.74-4.752-12.439-4.752-17.179,0c-4.74,4.764-4.74,12.475,0,17.227l99.876,99.888L3.555,220.497 c-4.74,4.704-4.74,12.319,0,17.011c4.74,4.704,12.439,4.704,17.179,0l100.152-99.599l99.551,99.563 c4.74,4.752,12.439,4.752,17.179,0c4.74-4.764,4.74-12.475,0-17.227L138.138,120.754z"/></svg></i></span>
+                                            <span class="cross"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 241.171 241.171" style="enable-background:new 0 0 241.171 241.171;" xml:space="preserve"><path id="Close" d="M138.138,120.754l99.118-98.576c4.752-4.704,4.752-12.319,0-17.011c-4.74-4.704-12.439-4.704-17.179,0 l-99.033,98.492L21.095,3.699c-4.74-4.752-12.439-4.752-17.179,0c-4.74,4.764-4.74,12.475,0,17.227l99.876,99.888L3.555,220.497 c-4.74,4.704-4.74,12.319,0,17.011c4.74,4.704,12.439,4.704,17.179,0l100.152-99.599l99.551,99.563 c4.74,4.752,12.439,4.752,17.179,0c4.74-4.764,4.74-12.475,0-17.227L138.138,120.754z"/></svg></span>
                                             
                                             <img 
                                                 :title="user.display_name"
@@ -497,6 +497,13 @@
     .actions-wrap {
         display: flex;
         flex-wrap: wrap;
+        justify-content: flex-start;
+
+        .pm-action-wrap {
+            display: flex;
+            justify-content: flex-start;
+            flex-wrap: wrap;
+        }
 
         .label {
             font-size: 13px;
