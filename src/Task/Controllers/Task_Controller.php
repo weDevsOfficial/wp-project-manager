3fe4bb72232c953d21e93e5548c5b80efa18412a
Asaquzzaman <joy.mishu@gmail.com>
2020-08-17 23:11:17 +0600

    Debugging

--- src/Task/Controllers/Task_Controller.php
+++ src/Task/Controllers/Task_Controller.php
@@ -228,7 +228,7 @@ class Task_Controller {
         $this->insert_type( $task->id, $type_id, $project_id, $board_id );
 
         //do_action( 'cpm_after_new_task', $task->id, $board_id, $project_id );
-        //do_action( 'pm_after_create_task', $task, $request->get_params() );
+        do_action( 'pm_after_create_task', $task, $request->get_params() );
 
         $resource = new Item( $task, new Task_Transformer );
 
