8a59a1f80e4a79849c6d4c669a2e9159925444c2
Asaquzzaman <joy.mishu@gmail.com>
2020-08-25 14:24:19 +0600

    Deploy v2.4.1

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.4.0',
+    'version'     => '2.4.1',
     'api'     	  => '2',
     'db_version'  => '2.4',
     'text_domain' => 'pm',
