e11faa00b6daf0800ee367cf19570879da517a76
Asaquzzaman <joy.mishu@gmail.com>
2020-09-08 12:33:08 +0600

    My-task project static url

--- views/assets/src/components/my-tasks/outstanding-task.vue
+++ views/assets/src/components/my-tasks/outstanding-task.vue
@@ -41,7 +41,7 @@
                 <tr v-if="tasks.length" v-for="task in tasks">
                     <td><a href="#" @click.prevent="popuSilgleTask(task)">{{ task.title }}</a></td>
                     <td>
-                        <router-link
+                        <!-- <router-link
                           :to="{
                             name: 'single_list',
                             params: {
@@ -50,10 +50,16 @@
                             }
                         }">
                             {{ task.task_list.data.title }}
-                        </router-link>
+                        </router-link> -->
+
+                        //For ERP Integration
+                        <a :href="`${PM_Vars.project_page}#/projects/${task.project_id}/task-lists/${task.task_list_id}`">
+                            {{ task.task_list.data.title }}
+                        </a>
+
                     </td>
                     <td>
-                        <router-link
+                       <!--  <router-link
                           :to="{
                             name: 'task_lists',
                             params: {
@@ -61,7 +67,12 @@
                             }
                         }">
                             {{ task.project_title }}
-                        </router-link>
+                        </router-link> -->
+                        
+                        //For ERP Integration
+                        <a :href="`${PM_Vars.project_page}#/projects/${task.project_id}/task-lists`">
+                            {{ task.project_title }}
+                        </a>
                     </td>
                     <td>{{ getOverdueValue(task) }}</td>
                 </tr>
