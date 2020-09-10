407a7afd10b295b7ff885c0a8f8fa8101d8353c9
Asaquzzaman <joy.mishu@gmail.com>
2020-09-10 09:31:03 +0600

    Remove unecessary code

--- views/assets/src/components/my-tasks/current-task.vue
+++ views/assets/src/components/my-tasks/current-task.vue
@@ -85,7 +85,7 @@
                             {{ task.project_title }}
                         </router-link> -->
                         
-                        //For ERP Integration
+                        <!-- For ERP Integration -->
                         <a :href="`${PM_Vars.project_page}#/projects/${task.project_id}/task-lists`">
                             {{ task.project_title }}
                         </a>
