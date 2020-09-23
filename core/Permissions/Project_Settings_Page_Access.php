dfea3e2c371317cef7ec38d7d901fe9694a773e9
Asaquzzaman <joy.mishu@gmail.com>
2020-09-23 16:49:53 +0600

    User access permission

--- core/Permissions/Project_Settings_Page_Access.php
+++ core/Permissions/Project_Settings_Page_Access.php
@@ -5,11 +5,12 @@ namespace WeDevs\PM\Core\Permissions;
 use WeDevs\PM\Core\Permissions\Abstract_Permission;
 use WP_REST_Request;
 
-class Project_Manage_Capability extends Abstract_Permission {
+class Project_Settings_Page_Access extends Abstract_Permission {
+    
     public function check() {
         $user_id    = get_current_user_id();
         $project_id = $this->request->get_param( 'project_id' );
-        
+
         if ( empty( $user_id ) || empty( $project_id ) ) {
             return new \WP_Error( 'project', __( "You have no permission.", "wedevs-project-manager" ) );
         }
