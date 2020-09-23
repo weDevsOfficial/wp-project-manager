9f69296affadd1eddb56a36efe2c1b9b27c6d1f0
Asaquzzaman <joy.mishu@gmail.com>
2020-09-23 11:08:56 +0600

    Fix project create capability

--- core/Permissions/Project_Create_Capability.php
+++ core/Permissions/Project_Create_Capability.php
@@ -8,12 +8,10 @@ use WP_REST_Request;
 class Project_Create_Capability extends Abstract_Permission {
     public function check() {
 
-        if ( pm_has_manage_capability() ) {
+        if ( pm_user_can_access( pm_manager_cap_slug() ) ) {
             return true;
         }
-        if ( pm_has_project_create_capability() ) {
-            return true;
-        }
+        
         return new \WP_Error( 'project', __( "You have no permission to create project.", "wedevs-project-manager" ) );
     }
 }
