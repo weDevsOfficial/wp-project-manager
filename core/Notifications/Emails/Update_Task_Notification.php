2de5024033538e431e6c21ab7f9dd259cd943d5d
Asaquzzaman <joy.mishu@gmail.com>
2020-10-15 14:22:37 +0600

    Update email logo

--- core/Notifications/Emails/Update_Task_Notification.php
+++ core/Notifications/Emails/Update_Task_Notification.php
@@ -16,8 +16,12 @@ class Update_Task_Notification extends Email {
 
     public function trigger( $task, $data ) {
 
+        $task_raw = pm_get_tasks( [ 'id' => $task->id ] );
+        $task_raw = $task_raw['data'];
+
         $task->load('assignees.assigned_user', 'projects.managers', 'updater');
         $users = array();
+        pm_log('task', $task_raw);
         foreach ($task->assignees->toArray() as $assignee ) {
             if( $this->is_enable_user_notification( $assignee['assigned_to'] ) ){
                 if( $this->is_enable_user_notification_for_notification_type( $assignee['assigned_to'] , '_cpm_email_notification_update_task' ) ){
@@ -39,18 +43,10 @@ class Update_Task_Notification extends Email {
         }
 
         $template_name = apply_filters( 'pm_new_task_email_template_path', $this->get_template_path( '/html/update-task.php' ) );
-        $subject = sprintf( __( '[%s][%s] Update Task Assigned: %s', 'wedevs-project-manager' ), $this->get_blogname(), $task->projects->title, $task->title );
+        $subject = sprintf( __( '[%s][%s] Update Task Assigned: %s', 'wedevs-project-manager' ), $this->get_blogname(), $task_raw['project_title'], $task_raw['title'] );
 
 
-        $message = $this->get_content_html( $template_name, [
-            'id'          => $task->id,
-            'title'       => $task->title,
-            'description' => $task->description,
-            'project_id'  => $task->project_id,
-            'updater'     => $task->updater->display_name,
-            'due_date'    => format_date( $task->due_date ),
-            'start_at'    => empty( $task->start_at ) ? null: format_date( $task->start ),
-        ] );
+        $message = $this->get_content_html( $template_name, $task_raw );
 
         $this->send( $users, $subject, $message );
 
--- views/emails/html/new-task.php
+++ views/emails/html/new-task.php
@@ -7,128 +7,131 @@
     <title>Your Message Subject or Title</title>
 </head>
 <body style="margin: 0; padding: 0; background: #ccc; font-family: 'Roboto', sans-serif; font-weight: 300; box-sizing: border-box;">
-    <table  align="center" border="1" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; background: #fff; border:1px solid #f2f2f2;">
-        <tr style="border-bottom: 1px solid #f2f2f2;">
-            <td style="padding: 50px;">
-                
-                <table  style="margin-right: 20px;">
-                    <tr>
-                        <td>
-                            <img 
-                                style="
-                                    outline: none; 
-                                    text-decoration: none; 
-                                    height: 48px; 
-                                    width: 48px; 
-                                    border-radius: 50%; 
-                                    outline: none;
-                                " 
-                                src="<?php echo esc_url( $creator['data']['avatar_url'] ); ?>" 
-                                alt="User Name" 
-                                title="User Name"
-                            />
-                        </td>
-                        <td>
-                            <div style="margin-left: 10px;">
-                                <h1 style="margin: 0 0 7px; font-weight: bold; font-size: 18px; color: #000000; letter-spacing: 0.16px; line-height: 22px;">
-                                    <?php echo esc_html( $creator['data']['display_name'] ); ?> <?php _e( 'assigned a task to you', 'wedevs-project-manager' ); ?>
-                                </h1>
-                                <a style="text-decoration: none; font-size: 15px; color: #0676D4; letter-spacing: 0.14px; line-height: 22px;" href="<?php echo esc_url($link.'#/projects/'.$project_id . '/task-lists/tasks/' . $id); ?>" target="_blank"><?php _e( 'View this task', 'wedevs-project-manger' ); ?></a> 
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-                
-            </td>
-        </tr>
-        <tr>
-            <td style="padding: 50px;">
-                <table>
-                    <tr>
-                        <td colspan="2">
-                            <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Task', 'wedevs-project-manager' ); ?></p>
-                            <h2 style="font-size: 18px; color: #000; margin: 0;"><?php echo esc_html( $title ); ?></h2>
-                            &nbsp;
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Assign To', 'wedevs-project-manager' ); ?></p>
-                            <p style="font-size: 16px; color: #000000; line-height: 30px; margin: 0;">
-                                <?php
-                                    $assign_users = [];
-                                    foreach( $assignees['data'] as $assign ) {
-                                        $assign_users[] = esc_html( $assign->display_name );
-                                    }
+    <center style="width: 100%;">
+        <table  align="center" border="1" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; background: #fff; border:1px solid #f2f2f2;">
+            <tr style="border-bottom: 1px solid #f2f2f2;">
+                <td style="padding: 50px;">
+                    
+                    <table  style="margin-right: 20px;">
+                        <tr>
+                            <td>
+                                <img 
+                                    style="
+                                        outline: none; 
+                                        text-decoration: none; 
+                                        height: 48px; 
+                                        width: 48px; 
+                                        border-radius: 50%; 
+                                        outline: none;
+                                    " 
+                                    src="<?php echo esc_url( $creator['data']['avatar_url'] ); ?>" 
+                                    alt="User Name" 
+                                    title="User Name"
+                                />
+                            </td>
+                            <td>
+                                <div style="margin-left: 10px;">
+                                    <h1 style="margin: 0 0 7px; font-weight: bold; font-size: 18px; color: #000000; letter-spacing: 0.16px; line-height: 22px;">
+                                        <?php echo esc_html( $creator['data']['display_name'] ); ?> <?php _e( 'assigned a task to you', 'wedevs-project-manager' ); ?>
+                                    </h1>
+                                    <a style="text-decoration: none; font-size: 15px; color: #0676D4; letter-spacing: 0.14px; line-height: 22px;" href="<?php echo esc_url($link.'#/projects/'.$project_id . '/task-lists/tasks/' . $id); ?>" target="_blank"><?php _e( 'View this task', 'wedevs-project-manger' ); ?></a> 
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 50px;">
+                    <table>
+                        <tr>
+                            <td colspan="2">
+                                <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Task', 'wedevs-project-manager' ); ?></p>
+                                <h2 style="font-size: 18px; color: #000; margin: 0;"><?php echo esc_html( $title ); ?></h2>
+                                &nbsp;
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Assign To', 'wedevs-project-manager' ); ?></p>
+                                <p style="font-size: 16px; color: #000000; line-height: 30px; margin: 0;">
+                                    <?php
+                                        $assign_users = [];
+                                        foreach( $assignees['data'] as $assign ) {
+                                            $assign_users[] = esc_html( $assign->display_name );
+                                        }
 
-                                    echo implode( ', ', $assign_users );
-                                ?>
-                            </p>
-                        </td>
-                        <td style="padding-left: 80px;">
-                            <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Due Date', 'wedevs-project-manager' ); ?></p>
-                            <p style="font-size: 16px; color: #000000; line-height: 30px; margin: 0;">
-                                <?php
+                                        echo implode( ', ', $assign_users );
+                                    ?>
+                                </p>
+                            </td>
+                            <td style="padding-left: 80px;">
+                                <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Due Date', 'wedevs-project-manager' ); ?></p>
+                                <p style="font-size: 16px; color: #000000; line-height: 30px; margin: 0;">
+                                    <?php
 
-                                    echo empty( $due_date['date'] ) ? '&#x02013;&#x02013;' : pm_date_format( esc_html( $due_date['date'] ) );
-                                ?>
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            &nbsp;
-                            <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Project', 'wedevs-project-manager' ); ?></p>
-                            <h2 style="font-size: 18px; color: #000; margin: 0;"><?php echo esc_html( $project_title ); ?></h2>
-                        </td>
-                    </tr>
+                                        echo empty( $due_date['date'] ) ? '&#x02013;&#x02013;' : pm_date_format( esc_html( $due_date['date'] ) );
+                                    ?>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                &nbsp;
+                                <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Project', 'wedevs-project-manager' ); ?></p>
+                                <h2 style="font-size: 18px; color: #000; margin: 0;"><?php echo esc_html( $project_title ); ?></h2>
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td colspan="2">
-                            &nbsp;
-                            <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Task description', 'wedevs-project-manager' ); ?></p>
-                            <div style="padding: 24px 30px; background: #edeef45e; border: 1px solid #f2f2f2; border-radius: 5px; margin-bottom: 30px; line-height: 26px; margin-top: 10px;">
-                                <?php echo empty( $description['html'] ) ? __( 'No description found!', 'wedevs-project-manager' ) : $description['html']; ?>
-                            </div>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td colspan="2">
+                                &nbsp;
+                                <p style="font-size: 16px; line-height: 30px; margin: 0; color: #7E8690;"><?php _e( 'Task description', 'wedevs-project-manager' ); ?></p>
+                                <div style="padding: 24px 30px; background: #edeef45e; border: 1px solid #f2f2f2; border-radius: 5px; margin-bottom: 30px; line-height: 26px; margin-top: 10px;">
+                                    <?php echo empty( $description['html'] ) ? __( 'No description found!', 'wedevs-project-manager' ) : $description['html']; ?>
+                                </div>
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td colspan="2">
-                            &nbsp;
-                            <a style="text-decoration: none; color: #0676D4; display: inline-block; padding: 9px 24px; font-size: 15px; color: #FFFFFF; letter-spacing: 0.14px; line-height: 30px; transition: opacity .2s; background: #7D60FF; border-radius: 3px;" href="<?php echo esc_url($link.'#/projects/'.$project_id . '/task-lists/tasks/' . $id); ?>" target="_blank">
-                                <?php _e( 'Reply', 'wedevs-project-manager'); ?>
-                            </a>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
+                        <tr>
+                            <td colspan="2">
+                                &nbsp;
+                                <a style="text-decoration: none; color: #0676D4; display: inline-block; padding: 9px 24px; font-size: 15px; color: #FFFFFF; letter-spacing: 0.14px; line-height: 30px; transition: opacity .2s; background: #7D60FF; border-radius: 3px;" href="<?php echo esc_url($link.'#/projects/'.$project_id . '/task-lists/tasks/' . $id); ?>" target="_blank">
+                                    <?php _e( 'View Task', 'wedevs-project-manager'); ?>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
 
-        <tr>
-            <td style="padding: 0 50px 50px;">
-                <div>
-                    <h2 style="font-size: 20px; color: #000000; letter-spacing: 0.18px; line-height: 30px; margin-bottom: 15px;">
-                        <?php _e( 'Collaborators', 'wedevs-project-manager'); ?>
-                    </h2>
-                    <span style="margin: 0; padding: 0; list-style: none; display: inline-block;">
+            <tr>
+                <td style="padding: 0 50px 50px;">
+                    <div>
+                        <h2 style="font-size: 20px; color: #000000; letter-spacing: 0.18px; line-height: 30px; margin-bottom: 15px;">
+                            <?php _e( 'Collaborators', 'wedevs-project-manager'); ?>
+                        </h2>
+                        <span style="margin: 0; padding: 0; list-style: none; display: inline-block;">
 
-                         <?php
+                             <?php
 
-                            foreach( $assignees['data'] as $assign ) {
-                                ?>
+                                foreach( $assignees['data'] as $assign ) {
+                                    ?>
 
-                                    <img style="outline:none; margin-right: 4px; text-decoration:none; height: 33px; width: 33px; border-radius: 50%;" src="<?php echo esc_url( $assign->avatar_url ); ?>" alt="User Name" title="User Name" width="33" height="33" />
-                                <?php
-                            }
+                                        <img style="outline:none; margin-right: 4px; text-decoration:none; height: 33px; width: 33px; border-radius: 50%;" src="<?php echo esc_url( $assign->avatar_url ); ?>" alt="User Name" title="User Name" width="33" height="33" />
+                                    <?php
+                                }
 
-                        ?>
-                
-                    </span>
-                </div>
-            </td>
-        </tr>
-        
-    </table>
+                            ?>
+                    
+                        </span>
+                    </div>
+                </td>
+            </tr>
+            
+        </table>
+    </center>
+    <?php require_once dirname(__FILE__) . '/footer.php'; ?>
 </body>
 </html>
