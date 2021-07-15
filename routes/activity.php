f1c3471686c62f1a818f32ea5dd756c18a557a91
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-07-15 08:48:56 +0600

    Fix My Task Was not showing & Update Calendar Library files (#416)
    * Added key to remove warning
    
    * Fix My Task List Data
    
    * Uncomment  condition
    
    * Updated calendar title and content styles
    
    * Updated FullCalendar library from 3.9.0 to 3.10.2
    
    * Fix Caledar Data refresh after creating my task
    
    * Fix My Task at a glace overview data reload after creating my task
    
    * Authentication implemented on missing un-authenticated routes

--- routes/activity.php
+++ routes/activity.php
@@ -4,10 +4,12 @@ use WeDevs\PM\Core\Router\Router;
 use WeDevs\PM\Core\Permissions\Administrator;
 use WeDevs\PM\Core\Permissions\Access_Project;
 
-$access_project =  '\WeDevs\PM\Core\Permissions\Access_Project';
+$access_project = '\WeDevs\PM\Core\Permissions\Access_Project';
+$authentic      = 'WeDevs\PM\Core\Permissions\Authentic';
 $router         = Router::singleton();
 
 $router->get( 'projects/{project_id}/activities', 'WeDevs/PM/Activity/Controllers/Activity_Controller@index' )
-    ->permission([$access_project]);
+    ->permission( [ $access_project ] );
 
-$router->get( 'activities', 'WeDevs/PM/Activity/Helper/Activity@get_activities' );
+$router->get( 'activities', 'WeDevs/PM/Activity/Helper/Activity@get_activities' )
+    ->permission( [ $authentic ] );
