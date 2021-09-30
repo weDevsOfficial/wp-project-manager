57d748f6e6e652d45851ecf7b1dac5052cc3654c
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-30 17:11:23 +0600

    Implmenting Sanitization for Category API Endpoints

--- routes/category.php
+++ routes/category.php
@@ -1,32 +1,29 @@
 <?php
 
 use WeDevs\PM\Core\Router\Router;
-use WeDevs\PM\Core\Permissions\Administrator;
-use WeDevs\PM\Core\Permissions\Authentic;
-use WeDevs\PM\Core\Permissions\Project_Manage_Capability;
 
-$router = Router::singleton();
+$router                    = Router::singleton();
 $project_manage_capability = 'WeDevs\PM\Core\Permissions\Project_Manage_Capability';
-$authentic = 'WeDevs\PM\Core\Permissions\Authentic';
+$authentic                 = 'WeDevs\PM\Core\Permissions\Authentic';
 
 $router->get( 'categories', 'WeDevs/PM/Category/Controllers/Category_Controller@index' )
-    ->permission([$authentic]);
+    ->permission( [ $authentic ] );
 
 $router->post( 'categories', 'WeDevs/PM/Category/Controllers/Category_Controller@store' )
-    ->permission(['WeDevs\PM\Core\Permissions\Categories_Page_Access'])
-    ->validator( 'WeDevs\PM\Category\Validators\Create_Category' );
-
+    ->permission( ['WeDevs\PM\Core\Permissions\Categories_Page_Access'] )
+    ->validator( 'WeDevs\PM\Category\Validators\Create_Category' )
+    ->sanitizer( 'WeDevs\PM\Category\Sanitizers\Category_Sanitizer' );
 
 $router->get( 'categories/{id}', 'WeDevs/PM/Category/Controllers/Category_Controller@show' )
-    ->permission([$authentic]);
+    ->permission( [ $authentic ] );
 
 $router->post( 'categories/{id}/update', 'WeDevs/PM/Category/Controllers/Category_Controller@update' )
-    ->permission(['WeDevs\PM\Core\Permissions\Categories_Page_Access'])
-    ->validator( 'WeDevs\PM\Category\Validators\Update_Category' );
+    ->permission( ['WeDevs\PM\Core\Permissions\Categories_Page_Access'] )
+    ->validator( 'WeDevs\PM\Category\Validators\Update_Category' )
+    ->sanitizer( 'WeDevs\PM\Category\Sanitizers\Category_Sanitizer' );
 
-    
 $router->post( 'categories/{id}/delete', 'WeDevs/PM/Category/Controllers/Category_Controller@destroy' )
-    ->permission(['WeDevs\PM\Core\Permissions\Categories_Page_Access']);
+    ->permission( ['WeDevs\PM\Core\Permissions\Categories_Page_Access'] );
 
 $router->post( 'categories/bulk-delete', 'WeDevs/PM/Category/Controllers/Category_Controller@bulk_destroy' )
-    ->permission(['WeDevs\PM\Core\Permissions\Categories_Page_Access']);
+    ->permission( ['WeDevs\PM\Core\Permissions\Categories_Page_Access'] );
