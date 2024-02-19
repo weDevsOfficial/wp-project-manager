c1c5b548cb50cbb7e86442401dc1b41b0d95f0df
MdAsifHossainNadim <nadim@wedevs.com>
2024-02-19 08:39:47 +0600

    fix: project-discussion-xss-security-error-&-bump-prev-version

--- src/Discussion_Board/Validators/Discussion_Board_Sanitizer.php
+++ src/Discussion_Board/Validators/Discussion_Board_Sanitizer.php
@@ -7,7 +7,7 @@ use WeDevs\PM\Core\Sanitizer\Abstract_Sanitizer;
 class Discussion_Board_Sanitizer extends Abstract_Sanitizer {
 	public function filters() {
         return [
-            'title'            => 'trimer|html_esc',
+            'title'            => 'trimer|sanitize_text_field',
             'description'      => 'trimer|pm_kses',
             'status'           => 'trimer',
         ];
