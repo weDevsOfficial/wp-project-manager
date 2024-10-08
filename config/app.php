d3e75ea245f45754a52d3aa21179c4884ded75fd
Rakibul 1971 <shazolislam8@gmail.com>
2024-10-08 18:27:37 +0600

    update plugin version and added changelog

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.6.13',
+    'version'     => '2.6.14',
     'api'     	  => '2',
     'db_version'  => '2.5',
     'text_domain' => 'pm',
