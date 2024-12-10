81bd9c87f29f678111b3c55a73d7d3b041337184
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2024-12-10 10:27:02 +0600

    version updated

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.6.15',
+    'version'     => '2.6.16',
     'api'     	  => '2',
     'db_version'  => '2.5',
     'text_domain' => 'pm',
