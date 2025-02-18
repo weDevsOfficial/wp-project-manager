f69a3b5c1a0e86555490ca2738797ab0d2477675
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-02-18 10:02:05 +0600

    Sanitizer Updated

--- src/Settings/Sanitizers/Task_Type_Sanitizer.php
+++ src/Settings/Sanitizers/Task_Type_Sanitizer.php
@@ -7,8 +7,8 @@ use WeDevs\PM\Core\Sanitizer\Abstract_Sanitizer;
 class Task_Type_Sanitizer extends Abstract_Sanitizer {
 	public function filters() {
         return [
-            'title'       => 'trimer|pm_kses',
-            'description' => 'trimer|pm_kses',
+            'title'       => 'trimer|sanitize_text_field',
+            'description' => 'trimer|sanitize_text_field',
         ];
     }
 }
