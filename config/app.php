18e8a4433806759cd944363be3f6667a94ac4bfa
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-09-04 19:36:12 +0600

    Added change log & bump version.

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.6.24',
+    'version'     => '2.6.25',
     'api'     	  => '2',
     'db_version'  => '2.5',
     'text_domain' => 'pm',
