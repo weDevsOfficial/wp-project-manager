36c28abc124aa5aae108051b02bd4a543bd07217
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-12-16 22:17:43 +0600

    WIP

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Project Manager',
     'slug'        => 'pm',
-    'version'     => '2.6.31',
+    'version'     => '3.0.0',
     'api'     	  => '2',
     'db_version'  => '2.5',
     'text_domain' => 'pm',
