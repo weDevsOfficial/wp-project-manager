44e03382b56308543fec29630aa14cde9a700e4c
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2026-01-14 12:18:36 +0600

    chore: bump version to v3.0.2

--- cpm.php
+++ cpm.php
@@ -5,7 +5,7 @@
  * Description: WordPress Project Management plugin. Manage your projects and tasks, get things done.
  * Author: weDevs
  * Author URI: https://wedevs.com
- * Version: 3.0.1
+ * Version: 3.0.2
  * Text Domain: wedevs-project-manager
  * Domain Path: /languages
  * License: GPL2
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 // Define version directly to avoid early translation loading from get_plugin_data()
-define('PM_VERSION', '3.0.1');
+define('PM_VERSION', '3.0.2');
 
 require __DIR__.'/bootstrap/loaders.php';
 require __DIR__.'/libs/configurations.php';
