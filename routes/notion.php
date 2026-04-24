f92a005b8304f894261e74c6fd3cc628e6bc7f7f
Ariful Hoque <hoquea57@gmail.com>
2026-04-24 11:52:09 +0600

    Require settings access permission for previews
    Replace the previous Authentic permission with Settings_Page_Access for GitHub and Notion preview endpoints. Updates the permission variable and applies it to both single and batch preview routes so previews require the Settings_Page_Access capability. Test-connection routes are unchanged.

--- routes/notion.php
+++ routes/notion.php
@@ -2,16 +2,16 @@
 
 use WeDevs\PM\Core\Router\Router;
 
-$wedevs_pm_router    = Router::singleton();
-$wedevs_pm_authentic = 'WeDevs\PM\Core\Permissions\Authentic';
+$wedevs_pm_router         = Router::singleton();
+$wedevs_pm_settings_access = 'WeDevs\PM\Core\Permissions\Settings_Page_Access';
 
 // Single Notion URL preview
 $wedevs_pm_router->post( 'notion/preview', 'WeDevs/PM/Notion/Controllers/Notion_Preview_Controller@preview' )
-    ->permission( [ $wedevs_pm_authentic ] );
+    ->permission( [ $wedevs_pm_settings_access ] );
 
 // Batch Notion URLs preview
 $wedevs_pm_router->post( 'notion/batch-preview', 'WeDevs/PM/Notion/Controllers/Notion_Preview_Controller@batch_preview' )
-    ->permission( [ $wedevs_pm_authentic ] );
+    ->permission( [ $wedevs_pm_settings_access ] );
 
 // Test Notion connection
 $wedevs_pm_router->post( 'notion/test-connection', 'WeDevs/PM/Notion/Controllers/Notion_Preview_Controller@test_connection' )
